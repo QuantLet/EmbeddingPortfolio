8259ba491630767bef8fcb40b2cd13474d7b66eb
BrunoSpilak <bruno.spilak@gmail.com>
2021-02-12 18:51:09 +0100

    adding more losses

--- dl_portfolio/config/config.py
+++ dl_portfolio/config/config.py
@@ -12,63 +12,90 @@ features = [
     {'name': 'returns', 'params': {'time_period': 1}}
 ]
 preprocess = {
-    'close': {'method': 'minmax', 'feature_range': [-1, 1]}
+    'close': {'method': 'seq_normalization', 'params': {'base': -1}}
 }
 
 # Model
-model_type = "EIIE"
+model_type = "asset_independent_model"
 layers = [
     {
-        'type': 'conv',
-        'filters': 2,
-        'kernel_size': [1, 3],
+        'type': 'lstm',
+        'neurons': 2,
         'params': {
-            'strides': (1, 1),
-            'activation': 'tanh'
+            'activation': 'tanh',
+            'return_sequences': True
+        }
+
+    },
+    {
+        'type': 'lstm',
+        'neurons': 1,
+        'params': {
+            'activation': 'linear'
         }
     },
     {
-        'type': 'EIIE_dense',
-        'filters': 10,
-        'params': {
-            'activation': 'tanh',
-            'kernel_regularizer': tf.keras.regularizers.L2(5e-9)
-        }
-    },
+        'type': 'simple_long_only'
+    }
 ]
 
-model_name = "EIIE_vol_pen_returns"
-n_hidden = 1
+
+# model_type = "EIIE"
+# layers = [
+#     {
+#         'type': 'conv2d',
+#         'filters': 2,
+#         'kernel_size': [1, 3],
+#         'params': {
+#             'strides': (1, 1),
+#             'activation': 'relu',
+#             'kernel_regularizer': tf.keras.regularizers.L2(5e-4)
+#         }
+#     },
+#     {
+#         'type': 'EIIE_dense',
+#         'filters': 8,
+#         'params': {
+#             'activation': 'relu',  'kernel_regularizer': tf.keras.regularizers.L2(5e-4)
+#         }
+#     },
+# ]
 dropout = 0
 
 # Training
-load_model = None
-optimizer = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.95)
-# optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
+# optimizer = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.8)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
 # TODO implement early stopping
-n_epochs = 100
+n_epochs = 150
 lr_scheduler = {
-    0: 0.01,
-    40: 0.001,
-    80: 0.0001
+    0: 0.01
 }
-batch_size = 16
+batch_size = 128
 log_every = 1
-plot_every = 10
+plot_every = 20
+cv_type='fold'
+load_model = 'CV'
 nb_folds = 5
-test_size = 300
+val_size = 6  # in months
 
 # Strategy, loss function
 freq = 14400  # 4h
 no_cash = True
 trading_fee = 0.0075
+
+loss_config = {
+    'name': 'cum_return',
+    'params': {'benchmark': 0.}
+}
 # loss_config = {
 #     'name': 'sharpe_ratio',
 #     'params': {'benchmark': 0., 'annual_period': 1}
 # }
-loss_config = {
-    'name': 'penalized_volatility_returns',
-    'params': {'benchmark': 0., 'alpha': 0.2}
-}
+# loss_config = {
+#     'name': 'penalized_volatility_returns',
+#     'params': {'benchmark': 0.0093, 'alpha': 0.8}
+# }
+
 # Output
-save = True
+model_name = "EIIE_SR"
+save = False
