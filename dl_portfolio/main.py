43f0e91aa76bad06e6fde102eac8a2959d59190e
BrunoSpilak <bruno.spilak@gmail.com>
2021-03-01 21:29:38 +0100

    cv log dir

--- dl_portfolio/main.py
+++ dl_portfolio/main.py
@@ -3,16 +3,16 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn import preprocessing
 from dl_portfolio.logger import LOGGER
+from dl_portfolio.metrics import np_portfolio_returns
 from dl_portfolio.model import build_mlp, build_mlp_with_cash_bias, EIIE_model, asset_independent_model
-from dl_portfolio.utils import create_log_dir
+from dl_portfolio.utils import create_log_dir, get_best_model_from_dir
 from dl_portfolio.data import build_delayed_window, features_generator, DataLoader, SeqDataLoader, reshape_to_2d_data
 from dl_portfolio.evaluate import plot_train_history
 from dl_portfolio.train import train
 from dl_portfolio.config import config
 import logging, pickle
-from dl_portfolio.benchmark import market_cap_returns
+from dl_portfolio.benchmark import market_cap_returns, equally_weighted_returns
 from shutil import copyfile
 
 ANN = ['mlp', 'mlp-cash-bias']
@@ -29,7 +29,7 @@ if __name__ == '__main__':
                  os.path.join(log_dir, 'config.py'))
 
     if config.seed is None:
-        seed = np.random.randint(0, 100)
+        seed = np.random.randint(0, 10)
         LOGGER.info(f'Set random seed {seed}')
         np.random.seed(seed)
     else:
@@ -41,6 +41,7 @@ if __name__ == '__main__':
     # if config.model_type in ANN:
     if config.model_type in ['EIIE', 'asset_independent_model']:
         data_loader = SeqDataLoader('EIIE', config.features, start_date=config.start_date, freq=config.freq,
+                                    path=config.path, pairs=config.pairs,
                                     seq_len=config.seq_len, val_size=config.val_size,
                                     preprocess_param=config.preprocess, batch_size=config.batch_size,
                                     lookfront=config.lookfront,
@@ -58,6 +59,10 @@ if __name__ == '__main__':
 
     for cv in range(config.nb_folds):
         LOGGER.info(f'CV {cv}')
+        if config.save:
+            cv_log_dir = os.path.join(log_dir, f'{cv}')
+        else:
+            cv_log_dir = None
         # create model
         LOGGER.info('Create model')
         if config.model_type == 'mlp':
@@ -79,7 +84,7 @@ if __name__ == '__main__':
                                dropout=config.dropout)
         elif config.model_type == 'asset_independent_model':
             LOGGER.info(f'Build {config.model_type} model')
-            if config.layers[0]['type'] in ['lstm', 'conv1d']:
+            if config.layers[0]['type'] in ['lstm', 'conv1d', 'gru']:
                 input_dim = (config.seq_len, data_loader.n_features)
             else:
                 input_dim = (data_loader.n_features)
@@ -91,7 +96,8 @@ if __name__ == '__main__':
 
         if config.load_model == 'CV':
             if cv > 0:
-                path = os.path.join(log_dir, f'model_{cv - 1}.ckpt')
+                model_file = get_best_model_from_dir(cv_log_dir)
+                path = os.path.join(cv_log_dir, model_file)
                 LOGGER.info(f'Loading pretrained model from {path}')
                 model.load_weights(path)
 
@@ -112,17 +118,17 @@ if __name__ == '__main__':
         LOGGER.info(f'Test data shape: {test_examples.shape}')
         LOGGER.info(f'Test returns shape: {test_returns.shape}')
 
-        if 'returns' in [f['name'] for f in config.features]:
-            for i in range(train_examples.shape[0]):
-                if config.lookfront > 0:
-                    assert (np.sum(
-                        train_examples[i, config.lookfront:, -1, -1] != train_returns.values[:-config.lookfront,
-                                                                        i])) == 0
-                else:
-                    assert (np.sum(
-                        train_examples[i, :, -1, -1] != train_returns.values[:, i])) == 0
-
-                print(True)
+        # if 'returns' in [f['name'] for f in config.features]:
+        #     for i in range(train_examples.shape[0]):
+        #         if config.lookfront > 0:
+        #             assert (np.sum(
+        #                 train_examples[i, config.lookfront:, -1, -1] != train_returns.values[:-config.lookfront,
+        #                                                                 i])) == 0
+        #         else:
+        #             assert (np.sum(
+        #                 train_examples[i, :, -1, -1] != train_returns.values[:, i])) == 0
+        #
+        #         print(True)
 
         # Training pipeline
         LOGGER.info('Create tf.data.Dataset')
@@ -144,18 +150,19 @@ if __name__ == '__main__':
                                                    config.optimizer, config.lr_scheduler, config.n_epochs,
                                                    data_loader.assets, config.trading_fee, config.log_every,
                                                    config.plot_every, no_cash=config.no_cash, clip_value=None,
-                                                   train_returns=train_returns, **config.loss_config['params'])
+                                                   train_returns=train_returns, **config.loss_config['params'],
+                                                   save=config.save, log_dir=cv_log_dir)
 
         # plot final history and save
         if config.save:
-            plot_train_history(train_history, test_history, save_dir=os.path.join(log_dir, f'history_{cv}.png'),
+            plot_train_history(train_history, test_history, save_dir=os.path.join(cv_log_dir, f'history_{cv}.png'),
                                show=True)
         else:
             plot_train_history(train_history, test_history, show=True)
 
         if config.save:
             # save model
-            model.save_weights(os.path.join(log_dir, f'model_{cv}.ckpt'))
+            model.save_weights(os.path.join(cv_log_dir, f'model_final.ckpt'))
 
         # Inference on train
         train_predictions = model.predict(features_generator(train_dataset, model_type=config.model_type),
@@ -168,7 +175,7 @@ if __name__ == '__main__':
         test_predictions = pd.DataFrame(test_predictions, columns=data_loader.assets, index=test_returns.index)
 
         if config.save:
-            pickle.dump(test_predictions, open(os.path.join(log_dir, f'test_prediction_{cv}.p'), 'wb'))
+            pickle.dump(test_predictions, open(os.path.join(cv_log_dir, f'test_prediction.p'), 'wb'))
 
         # Final prediction
         fig, axs = plt.subplots(1, 2, figsize=(15, 3))
@@ -179,28 +186,61 @@ if __name__ == '__main__':
         axs[1].legend(data_loader.assets)
         axs[1].set_title('Test prediction')
         if config.save:
-            plt.savefig(os.path.join(log_dir, f'prediction_{cv}.png'))
+            plt.savefig(os.path.join(cv_log_dir, f'prediction.png'))
         plt.show()
 
         # Final perf
         fig, axs = plt.subplots(1, 2, figsize=(15, 3))
-        strat_perf = (train_returns * train_predictions).sum(1)
+        if config.no_cash:
+            initial_position = np.array([[1 / data_loader.n_assets] * data_loader.n_assets])
+        else:
+            initial_position = np.array(np.array([[0] * (data_loader.n_assets - 1)]))
+        strat_perf_no_fee, strat_perf = np_portfolio_returns(train_predictions, train_returns,
+                                                             initial_position=initial_position,
+                                                             trading_fee=config.trading_fee,
+                                                             cash_bias=not config.no_cash)
         train_returns = pd.DataFrame(train_returns, index=train_predictions.index,
                                      columns=train_predictions.columns)
-        benchmark_returns, benchmark_value = market_cap_returns(train_returns, config.freq)
+        if config.benchmark == 'marketcap':
+            benchmark_returns, benchmark_value = market_cap_returns(train_returns, config.freq)
+        elif config.benchmark == 'equally_weighted':
+            benchmark_returns, benchmark_value = equally_weighted_returns(train_returns)
+        else:
+            raise NotImplementedError(f"Benchmark '{config.benchmark}' not implemented")
+
         axs[0].plot(benchmark_value, label='benchmark')
-        axs[0].plot((strat_perf + 1).cumprod(), label='strategy')
+        axs[0].plot(np.exp(np.cumsum(strat_perf)), label='strategy')
         axs[0].legend()
         axs[0].set_title('Final train performance')
 
-        strat_perf = (test_returns * test_predictions).sum(1)
+        if config.no_cash:
+            initial_position = train_predictions.iloc[-1:, :]
+        else:
+            initial_position = train_predictions.iloc[-1:, :-1]
+        strat_perf_no_fee, strat_perf = np_portfolio_returns(test_predictions, test_returns,
+                                                             initial_position=initial_position,
+                                                             trading_fee=config.trading_fee,
+                                                             cash_bias=not config.no_cash)
         test_returns = pd.DataFrame(test_returns, index=test_predictions.index,
                                     columns=train_predictions.columns)
-        benchmark_returns, benchmark_value = market_cap_returns(test_returns, config.freq)
+        if config.benchmark == 'marketcap':
+            benchmark_returns, benchmark_value = market_cap_returns(test_returns, config.freq, trading_fee=config.trading_fee)
+        elif config.benchmark == 'equally_weighted':
+            benchmark_returns, benchmark_value = equally_weighted_returns(test_returns, trading_fee=config.trading_fee)
+        else:
+            raise NotImplementedError(f"Benchmark '{config.benchmark}' not implemented")
         axs[1].plot(benchmark_value, label='benchmark')
-        axs[1].plot((strat_perf + 1).cumprod(), label='strategy')
+        axs[1].plot()
+
+        axs[1].plot(np.exp(np.cumsum(strat_perf)), label='strategy')
+        axs[1].plot(np.exp(np.cumsum(strat_perf_no_fee)), label='strategy no fee')
         axs[1].legend()
         axs[1].set_title('Final test performance')
         if config.save:
-            plt.savefig(os.path.join(log_dir, f'performance_{cv}.png'))
+            plt.savefig(os.path.join(cv_log_dir, f'performance.png'))
+        plt.show()
+        plt.plot(np.exp(np.cumsum(strat_perf - benchmark_returns)) - 1)
+        plt.title('Excess return')
+        if config.save:
+            plt.savefig(os.path.join(cv_log_dir, f'excess_performance.png'))
         plt.show()
