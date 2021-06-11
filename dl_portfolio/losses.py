0658296f2fc7282b7a029b2af712f973037b767e
BrunoSpilak <bruno.spilak@gmail.com>
2021-06-11 16:03:58 +0200

    fix weighted losses

--- dl_portfolio/losses.py
+++ dl_portfolio/losses.py
@@ -5,15 +5,25 @@ import numpy as np
 from tensorflow.python.ops import math_ops
 
 
-def weighted_mse(y_true, y_pred, weights):
+def weighted_mse(y_true, y_pred, weights=None):
+    y_true = tf.cast(y_true, dtype=tf.float32)
+    y_pred = tf.cast(y_pred, dtype=tf.float32)
+    weights = tf.cast(weights, dtype=tf.float32)
     diff = tf.math.squared_difference(y_pred, y_true)  # squared difference
-    weighted_diff = tf.math.multiply(weights, diff)  # weights * diff
-    loss = K.mean(weighted_diff, axis=-1)  # mean over last dimension
+    if weights is not None:
+        diff = tf.math.multiply(weights, diff)  # weights * diff
+    loss = K.mean(diff, axis=-1)  # mean over last dimension: mean of individual loss for each target
+    loss = K.mean(loss, axis=0)  # mean over batch
     return loss
 
 
-def weighted_mae(y_true, y_pred, weights):
+def weighted_mae(y_true, y_pred, weights=None):
+    y_true = tf.cast(y_true, dtype=tf.float32)
+    y_pred = tf.cast(y_pred, dtype=tf.float32)
+    weights = tf.cast(weights, dtype=tf.float32)
     diff = tf.math.abs(y_pred - y_true)  # squared difference
-    weighted_diff = tf.math.multiply(weights, diff)  # weights * diff
-    loss = K.mean(weighted_diff, axis=-1)  # mean over last dimension
+    if weights is not None:
+        diff = tf.math.multiply(weights, diff)  # weights * diff
+    loss = K.mean(diff, axis=-1)  # mean over last dimension: mean of individual loss for each target
+    loss = K.mean(loss, axis=0)  # mean over batch
     return loss
