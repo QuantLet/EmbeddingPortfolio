b06c8d2a7d944c5eed48b543c740fe285e689149
BrunoSpilak <bruno.spilak@gmail.com>
2021-06-20 21:20:46 +0200

    update config

--- dl_portfolio/config/ae_config.py
+++ dl_portfolio/config/ae_config.py
@@ -8,26 +8,34 @@ from dl_portfolio.constraints import (UncorrelatedFeaturesConstraint, NonNegAndU
 # seed = np.random.randint(100)
 data_type = ['indices', 'forex', 'forex_metals', 'commodities', 'crypto']
 shuffle_columns = False  # True
+dropnan = False
+freq = "1H"
 drop_weekends = False
 shuffle_columns_while_training = False
+scaler_func = {
+    'name': 'StandardScaler'
+}
+# features_config=None
+features_config = [{'name': 'hour_in_week'}]
+activation = 'relu'
 model_type = 'pca_ae_model'
 seed = np.random.randint(0, 100)
 fx = True
-save = True
-model_name = f'encoding5_weighted_mse_var_0.05'
+save = False
+model_name = f'RELU_encoding5_mse_time_feature_weightage_1'
 encoding_dim = 5
 learning_rate = 1e-3
 epochs = 1000
 batch_size = 256
 drop_remainder_obs = True
-activation = 'elu'
 val_size = 30 * 3 * 24
-loss = 'weighted_mse'
-label_param = {
-    'lq': 0.05,
-    'uq': 0.95,
-    'window': 2 * 24
-}
+loss = 'mse'
+label_param = None
+# label_param = {
+#     'lq': 0.05,
+#     'uq': 0.95,
+#     'window': 24 * 7
+# }
 # cov_loss = 'mse_with_covariance_penalty'
 rescale = None
 
@@ -39,7 +47,7 @@ activity_regularizer = UncorrelatedFeaturesConstraint(encoding_dim, norm='1/2', 
 # activity_regularizer = TailUncorrelatedFeaturesConstraint(encoding_dim, q=0.05, side='left', norm='1/2', weightage=1000.)
 callback_activity_regularizer = False
 kernel_initializer = NonNegAndUnitNormInit(initializer='glorot_uniform')
-kernel_regularizer = WeightsOrthogonalityConstraint(encoding_dim, weightage=1e-2, axis=0)
+kernel_regularizer = WeightsOrthogonalityConstraint(encoding_dim, weightage=1., axis=0)
 kernel_constraint = NonNegAndUnitNorm(axis=0)  # tf.keras.constraints.NonNeg()#
 
 
