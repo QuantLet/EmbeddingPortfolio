891cb9c26beb15b8daabf38258a6bb7e298705c1
BrunoSpilak <bruno.spilak@gmail.com>
2021-12-03 13:06:23 +0100

    adding pytest

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ setup(
         "riskparityportfolio==0.2",
         "joblib==1.0.0",
         "scikit-learn==0.24.0",
-        "fastcluster>=1.2.3"
+        "fastcluster>=1.2.3",
+        "pytest==6.2.5"
     ],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
