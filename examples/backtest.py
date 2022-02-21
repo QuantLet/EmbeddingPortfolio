82a900dfb35086c52486c6d988c866f99a5086b6
BrunoSpilak <bruno.spilak@gmail.com>
2022-02-21 23:24:37 +0100

    minor

--- examples/backtest.py
+++ examples/backtest.py
@@ -1,21 +1,36 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import os, pickle
-import numpy as np
-from dl_portfolio.logger import LOGGER
-from dl_portfolio.backtest import cv_portfolio_perf, get_cv_results, bar_plot_weights, backtest_stats, plot_perf, \
-    get_average_perf, get_ts_weights
 import datetime as dt
-import sys, logging
-from dl_portfolio.ae_data import load_data
+import logging
+import os
+import pickle
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn import metrics, preprocessing
+
+from dl_portfolio.backtest import cv_portfolio_perf, bar_plot_weights, backtest_stats, plot_perf, \
+    get_average_perf, get_ts_weights, get_cv_results, get_dl_average_weights
+from dl_portfolio.cluster import get_cluster_labels, consensus_matrix, rand_score_permutation, \
+    assign_cluster_from_consmat
+from dl_portfolio.evaluate import pred_vs_true_plot, average_prediction, average_prediction_cv
+from dl_portfolio.logger import LOGGER
+from dl_portfolio.constant import BASE_FACTOR_ORDER_RAFFINOT, BASE_FACTOR_ORDER_BOND
 
 # PORTFOLIOS = ['equal', 'markowitz', 'ae_ivp', 'hrp', 'hcaa', 'ae_rp', 'ae_rp_c', 'aeaa', 'kmaa']
 # STRAT = ['equal', 'markowitz', 'aerp', 'hrp', 'hcaa', 'aeerc', 'ae_rp_c', 'aeaa', 'kmaa']
-PORTFOLIOS = ['equal', 'ae_ivp', 'hrp', 'hcaa', 'ae_rp', 'ae_rp_c', 'aeaa', 'kmaa']
-STRAT = ['equal', 'aerp', 'hrp', 'hcaa', 'aeerc', 'ae_rp_c', 'aeaa', 'kmaa']
 
-# PORTFOLIOS = ['ae_rp_c']
-# STRAT = ['ae_rp_c']
+# AE bond
+# PORTFOLIOS = ['equal', 'equal_class', 'markowitz', 'ae_ivp', 'hrp', 'hcaa', 'ae_rp', 'ae_rp_c', 'aeaa', 'kmaa']
+# STRAT = ['equal', 'equal_class', 'markowitz', 'aerp', 'hrp', 'hcaa', 'aeerc', 'ae_rp_c', 'aeaa', 'kmaa']
+
+# AE raffinot
+PORTFOLIOS = ['equal', 'equal_class', 'ae_ivp', 'hrp', 'hcaa', 'ae_rp', 'ae_rp_c', 'aeaa', 'kmaa']
+STRAT = ['equal', 'equal_class', 'aerp', 'hrp', 'hcaa', 'aeerc', 'ae_rp_c', 'aeaa', 'kmaa']
+
+# PORTFOLIOS = ['equal', 'equal_class', 'ae_ivp', 'ae_rp', 'ae_rp_c', 'aeaa']
+# STRAT = ['equal', 'equal_class', 'aerp', 'aeerc', 'ae_rp_c', 'aeaa']
 
 if __name__ == "__main__":
     import argparse, json
@@ -24,9 +39,10 @@ if __name__ == "__main__":
     parser.add_argument("--base_dir",
                         type=str,
                         help="Experiments dir")
-    parser.add_argument("--dataset",
+    parser.add_argument("--model_type",
+                        default='ae',
                         type=str,
-                        help="Dataset name")
+                        help="ae or nmf")
     parser.add_argument("--test_set",
                         default='val',
                         type=str,
@@ -45,6 +61,9 @@ if __name__ == "__main__":
     parser.add_argument("--save",
                         action='store_true',
                         help="Save results")
+    parser.add_argument("--legend",
+                        action='store_true',
+                        help="Add legend to plots")
     parser.add_argument("-v",
                         "--verbose",
                         help="Be verbose",
@@ -62,8 +81,39 @@ if __name__ == "__main__":
     args = parser.parse_args()
     logging.basicConfig(level=args.loglevel)
     LOGGER.setLevel(args.loglevel)
+    meta = vars(args)
+    if args.save:
+        save_dir = f"backtest/{args.test_set}_{args.base_dir}" + '_' + dt.datetime.now().strftime("%Y%m%d_%H%M%S")
+        if not os.path.isdir(save_dir):
+            os.makedirs(save_dir)
+        LOGGER.info(f"Saving result to {save_dir}")
+        # os.makedirs(f"{save_dir}/cv_plots/")
 
-    if args.dataset == 'bond':
+        meta['save_dir'] = save_dir
+        json.dump(meta, open(f"{save_dir}/meta.json", "w"))
+
+    EVALUATION = {'model': {}, 'cluster': {}}
+
+    # Load paths
+    models = os.listdir(args.base_dir)
+    paths = [f"{args.base_dir}/{d}" for d in models if os.path.isdir(f"{args.base_dir}/{d}") and d[0] != "."]
+    n_folds = os.listdir(paths[0])
+    n_folds = sum([d.isdigit() for d in n_folds])
+    sys.path.append(paths[0])
+
+    if args.model_type == "ae":
+        import ae_config as config
+
+        assert "ae" in config.model_type
+    elif args.model_type == "nmf":
+        import nmf_config as config
+
+        assert "nmf" in config.model_type
+    else:
+        raise ValueError(f"run '{args.run}' is not implemented. Shoule be 'ae' or 'kmeans' or 'nmf'")
+
+    # Load Market budget
+    if config.dataset == 'bond':
         market_budget = pd.read_csv('data/market_budget_bond.csv', index_col=0)
         cryptos = ['BTC', 'DASH', 'ETH', 'LTC', 'XRP']
         market_budget = pd.concat([market_budget, pd.DataFrame(np.array([['crypto', 1]] * len(cryptos)),
@@ -71,38 +121,28 @@ if __name__ == "__main__":
                                                                columns=market_budget.columns)])
         # market_budget = market_budget.drop('CRIX')
         market_budget['rc'] = market_budget['rc'].astype(int)
-    elif args.dataset in ["raffinot_multi_asset", "raffinot_bloomberg_comb_update_2021"]:
+    elif config.dataset in ["raffinot_multi_asset", "raffinot_bloomberg_comb_update_2021"]:
         market_budget = pd.read_csv('data/market_budget_raffinot_multiasset.csv', index_col=0)
         market_budget['rc'] = market_budget['rc'].astype(int)
+    elif config.dataset == 'cac':
+        market_budget = pd.read_csv('data/market_budget_cac.csv', index_col=0)
     else:
         raise NotImplementedError()
 
-    meta = vars(args)
-    if args.save:
-        save_dir = f"eval_backtest/{args.test_set}_{args.base_dir}" + '_' + dt.datetime.now().strftime("%Y%m%d_%H%M%S")
-        if not os.path.isdir(save_dir):
-            os.makedirs(save_dir)
-        LOGGER.info(f"Saving result to {save_dir}")
-        # os.makedirs(f"{save_dir}/cv_plots/")
-        meta['save_dir'] = save_dir
-        json.dump(meta, open(f"{save_dir}/meta.json", "w"))
+    if config.dataset == "bond":
+        CLUSTER_NAMES = BASE_FACTOR_ORDER_BOND
+    elif config.dataset == "raffinot_bloomberg_comb_update_2021":
+        CLUSTER_NAMES = BASE_FACTOR_ORDER_RAFFINOT
+    else:
+        raise NotImplementedError()
 
-    models = os.listdir(args.base_dir)
-    paths = [f"{args.base_dir}/{d}" for d in models if d[0] != '.']
-
-    n_folds = os.listdir(paths[0])
-    n_folds = sum([d.isdigit() for d in n_folds])
-
-    sys.path.append(paths[0])
-    import ae_config
-
-
-    #####
+    # Main loop to get results
     cv_results = {}
     train_cov = {}
     test_cov = {}
     port_perf = {}
 
+    LOGGER.info("Starting main loop...")
     for i, path in enumerate(paths):
         LOGGER.info(len(paths) - i)
         if i == 0:
@@ -112,30 +152,52 @@ if __name__ == "__main__":
         cv_results[i] = get_cv_results(path,
                                        args.test_set,
                                        n_folds,
-                                       dataset=args.dataset,
+                                       dataset=config.dataset,
                                        portfolios=portfolios,
                                        market_budget=market_budget,
                                        window=args.window,
                                        n_jobs=args.n_jobs,
-                                       ae_config=ae_config)
-        port_perf[i] = cv_portfolio_perf(cv_results[i], portfolios=portfolios, annualized=True)
+                                       ae_config=config)
+    LOGGER.info("Done.")
 
-    dates = [cv_results[0][cv]['test_features'].index[0] for cv in cv_results[0]]
+    # Get average weights for AE portfolio across runs
+    port_weights = get_dl_average_weights(cv_results)
+    # Build dictionary for cv_portfolio_perf
+    cv_returns = {}
+    for cv in cv_results[0]:
+        cv_returns[cv] = cv_results[0][cv]['returns'].copy()
+        date = cv_results[0][cv]['returns'].index[0]
+        for port in PORTFOLIOS:
+            if port not in ['equal', 'equal_class'] and 'ae' not in port:
+                weights = pd.DataFrame(cv_results[0][cv]['port'][port]).T
+                weights.index = [date]
+                port_weights[cv][port] = weights
+    cv_portfolio = {
+        cv: {
+            'returns': cv_returns[cv],
+            'train_returns': cv_results[0][cv]['train_returns'],
+            'port': {port: port_weights[cv][port].values for port in port_weights[cv]
+                     # if port not in ['equal', 'equal_classs']
+                     }
+        } for cv in cv_returns
+    }
+    port_perf, leverage = cv_portfolio_perf(cv_portfolio, portfolios=PORTFOLIOS, volatility_target=0.05,
+                                            market_budget=market_budget)
+
+    K = cv_results[i][0]['loading'].shape[-1]
+    CV_DATES = [str(cv_results[0][cv]['returns'].index[0].date()) for cv in range(n_folds)]
     ASSETS = list(cv_results[i][0]['returns'].columns)
 
-    # Weights
-    port_weights = {}
-    for p in PORTFOLIOS:
-        if p != 'equal':
-            port_weights[p] = get_ts_weights(cv_results, port=p)
-
+    # Get portfolio weights time series
+    # port_weights = {}
+    # for p in PORTFOLIOS:
+    #     if p not in ['equal', 'equal_class']:
+    #         port_weights[p] = get_ts_weights(cv_results, port=p)
+    port_weights = get_ts_weights(port_weights)
     # Get average perf across runs
     ann_perf = pd.DataFrame()
     for p in PORTFOLIOS:
-        if 'ae' in p:
-            ann_perf[p] = get_average_perf(port_perf, port=p)
-        else:
-            ann_perf[p] = port_perf[0][p]['total'][0]
+        ann_perf[p] = port_perf[p]['total'].iloc[:, 0]
 
     # Some renaming
     port_weights['aerp'] = port_weights['ae_ivp'].copy()
@@ -147,117 +209,21 @@ if __name__ == "__main__":
     port_weights.pop('ae_rp')
     ann_perf.drop(['ae_ivp', 'ae_rp'], axis=1, inplace=True)
 
-    # Plot perf
+    LOGGER.info("Get cluster assignment...")
+    cv_labels = {}
+    for cv in range(n_folds):
+        cv_labels[cv] = {}
+        for i in cv_results:
+            c, cv_labels[cv][i] = get_cluster_labels(cv_results[i][cv]['embedding'])
+
+    cluster_assignment = {}
+    for cv in cv_labels:
+        cons_mat = consensus_matrix(cv_labels[cv], reorder=True, method="single")
+        cluster_assignment[cv] = assign_cluster_from_consmat(cons_mat, CLUSTER_NAMES, t=0)
+    LOGGER.info("Done.")
     if args.save:
         LOGGER.info('Saving performance... ')
         ann_perf.to_csv(f"{save_dir}/portfolios_returns.csv")
+        leverage.to_csv(f"{save_dir}/leverage.csv")
         pickle.dump(port_weights, open(f"{save_dir}/portfolios_weights.p", "wb"))
-
-        plot_perf(ann_perf, strategies=STRAT,
-                  save_path=f"{save_dir}/performance_all.png",
-                  show=args.show, legend=True)
-        plot_perf(ann_perf, strategies=[p for p in STRAT if p not in ['aerp', 'aeerc']],
-                  save_path=f"{save_dir}/performance_ae_rp_c_vs_all.png",
-                  show=args.show, legend=True)
-        plot_perf(ann_perf, strategies=['hrp', 'aerp'], save_path=f"{save_dir}/performance_hrp_aerp.png",
-                  show=args.show, legend=True)
-        plot_perf(ann_perf, strategies=['hcaa', 'aeerc'], save_path=f"{save_dir}/performance_hcaa_aeerc.png",
-                  show=args.show, legend=True)
-        plot_perf(ann_perf, strategies=['hrp', 'ae_rp_c'], save_path=f"{save_dir}/performance_hrp_aeerc_cluster.png",
-                  show=args.show, legend=True)
-        plot_perf(ann_perf, strategies=['hcaa', 'ae_rp_c'], save_path=f"{save_dir}/performance_hcaa_aeerc_cluster.png",
-                  show=args.show, legend=True)
-        if 'markowitz' in PORTFOLIOS:
-            plot_perf(ann_perf, strategies=['markowitz', 'ae_rp_c'],
-                  save_path=f"{save_dir}/performance_markowitz_aeerc_cluster.png",
-                  show=args.show, legend=True)
-
-        if 'shrink_markowitz' in PORTFOLIOS:
-            bar_plot_weights(port_weights['shrink_markowitz'], save_path=f"{save_dir}/weights_shrink_markowitz.png",
-                             show=args.show)
-        if 'markowitz' in PORTFOLIOS:
-            bar_plot_weights(port_weights['markowitz'], save_path=f"{save_dir}/weights_markowitz.png", show=args.show)
-        bar_plot_weights(port_weights['hrp'], save_path=f"{save_dir}/weights_hrp.png", show=args.show)
-        bar_plot_weights(port_weights['aerp'], save_path=f"{save_dir}/weights_aerp.png", show=args.show)
-        bar_plot_weights(port_weights['hcaa'], save_path=f"{save_dir}/weights_hcaa.png", show=args.show)
-        bar_plot_weights(port_weights['aeerc'], save_path=f"{save_dir}/weights_aeerc.png", show=args.show)
-        bar_plot_weights(port_weights['hrp'], save_path=f"{save_dir}/weights_hrp.png", show=args.show)
-        bar_plot_weights(port_weights['ae_rp_c'], save_path=f"{save_dir}/weights_aeerc_cluster.png", show=args.show)
-        bar_plot_weights(port_weights['aeaa'], save_path=f"{save_dir}/weights_aeaa.png", show=args.show)
-    else:
-        plot_perf(ann_perf, strategies=STRAT, show=args.show, legend=True)
-        plot_perf(ann_perf, strategies=['hrp', 'aerp'], show=args.show, legend=True)
-        bar_plot_weights(port_weights['hrp'], show=args.show)
-        bar_plot_weights(port_weights['aerp'], show=args.show)
-
-        plot_perf(ann_perf, strategies=['hcaa', 'aeerc'], show=args.show, legend=True)
-        bar_plot_weights(port_weights['hcaa'], show=args.show)
-        bar_plot_weights(port_weights['aeerc'], show=args.show)
-
-        plot_perf(ann_perf, strategies=['hrp', 'ae_rp_c'], show=args.show, legend=True)
-        bar_plot_weights(port_weights['hrp'], show=args.show)
-        bar_plot_weights(port_weights['ae_rp_c'], show=args.show)
-        bar_plot_weights(port_weights['aeaa'], show=args.show)
-
-    # Plot excess return
-    plt.figure(figsize=(20, 10))
-    plt.plot(np.cumprod(ann_perf['aerp'] + 1) - np.cumprod(ann_perf['hrp'] + 1))
-    if args.save:
-        plt.savefig(f"{save_dir}/excess_performance_hrp_aerp.png", bbox_inches='tight')
-
-    plt.figure(figsize=(20, 10))
-    plt.plot(np.cumprod(ann_perf['aeerc'] + 1) - np.cumprod(ann_perf['hcaa'] + 1))
-    if args.save:
-        plt.savefig(f"{save_dir}/excess_performance_hcaa_aeerc.png", bbox_inches='tight')
-
-    plt.figure(figsize=(20, 10))
-    plt.plot(np.cumprod(ann_perf['ae_rp_c'] + 1) - np.cumprod(ann_perf['hcaa'] + 1))
-    if args.save:
-        plt.savefig(f"{save_dir}/excess_performance_hcaa_aeerc_cluster.png", bbox_inches='tight')
-
-    plt.figure(figsize=(20, 10))
-    plt.plot(np.cumprod(ann_perf['ae_rp_c'] + 1) - np.cumprod(ann_perf['hrp'] + 1))
-    if args.save:
-        plt.savefig(f"{save_dir}/excess_performance_hrp_aeerc_cluster.png", bbox_inches='tight')
-
-    if 'markowitz' in PORTFOLIOS:
-        plt.figure(figsize=(20, 10))
-        plt.plot(np.cumprod(ann_perf['ae_rp_c'] + 1) - np.cumprod(ann_perf['markowitz'] + 1))
-        if args.save:
-            plt.savefig(f"{save_dir}/excess_performance_markowitz_aeerc_cluster.png", bbox_inches='tight')
-
-    # Plot one cv weight
-    cv = 0
-    date = str(port_weights['hrp'].index[cv].date())
-    plt.figure(figsize=(14, 7))
-    plt.bar(ASSETS, port_weights['hrp'].iloc[cv].values, label='hrp')
-    plt.bar(ASSETS, port_weights['aerp'].iloc[cv].values, label='aerp')
-    plt.legend()
-    plt.ylim([0, 0.9])
-    x = plt.xticks(rotation=45)
-    if args.save:
-        plt.savefig(f"{save_dir}/weights_hrp_aerp.png", bbox_inches='tight')
-
-    plt.figure(figsize=(14, 7))
-    plt.bar(ASSETS, port_weights['hcaa'].iloc[cv].values, label='hcaa')
-    plt.bar(ASSETS, port_weights['aeerc'].iloc[cv].values, label='aeerc')
-    plt.legend()
-    plt.ylim([0, 0.9])
-    x = plt.xticks(rotation=45)
-    if args.save:
-        plt.savefig(f"{save_dir}/weights_hcaa_aeerc.png", bbox_inches='tight')
-
-    plt.figure(figsize=(14, 7))
-    plt.bar(ASSETS, port_weights['hrp'].iloc[cv].values, label='hrp')
-    plt.bar(ASSETS, port_weights['ae_rp_c'].iloc[cv].values, label='ae_rp_c')
-    plt.legend()
-    plt.ylim([0, 0.9])
-    x = plt.xticks(rotation=45)
-    if args.save:
-        plt.savefig(f"{save_dir}/weights_hrp_aeerc_cluster.png", bbox_inches='tight')
-
-    # Get statistics
-    stats = backtest_stats(ann_perf, port_weights, period=252, format=True)
-    if args.save:
-        stats.to_csv(f"{save_dir}/backtest_stats.csv")
-    print(stats.to_string())
+        pickle.dump(cluster_assignment, open(f"{save_dir}/cluster_assignment.p", "wb"))
