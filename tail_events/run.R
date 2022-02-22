7e32369cce27ba172907e39c0599abf9203d2e61
BrunoSpilak <bruno.spilak@gmail.com>
2022-02-22 10:02:09 +0100

    Creating tail_events.R

--- tail_events/run.R
+++ tail_events/run.R
@@ -1,174 +1,133 @@
-rm(list = ls(all = TRUE))
-graphics.off()
 source("utils.R")
-library(rjson)
 
-# ------------------------ Input ------------------------
-# Input
-config_path = "./config/dataset1.json"
-save = TRUE
-save_dir = "output"
-debug = FALSE
-
-
-# ------------------------ Script ------------------------
-
-config = fromJSON(file = config_path)
-run = config$run
-if (run == "train") {
-  data_path = config$datapath$train
-}
-if (run == "test") {
-  data_path = config$datapath$test
-}
-
-if (save) {
-  dataset = strsplit(config_path, "/")[[1]][length(strsplit(config_path, "/")[[1]])]
-  dataset = strsplit(dataset, ".json")[[1]]
-  cwd = getwd()
-  save_dir = file.path(cwd, save_dir)
-  if (!dir.exists(save_dir)) {
-    dir.create(save_dir)
+run = function(config, save_dir=NULL, debug=FALSE){
+  if (is.null(save_dir)){
+    save = FALSE
+  } else {
+    save = TRUE
   }
-  save_dir = file.path(save_dir, dataset)
-  if (!dir.exists(save_dir)) {
-    dir.create(save_dir)
+  if (config$run == "train") {
+    data_path = config$datapath$train
   }
-  save_dir = file.path(save_dir, run)
-  if (!dir.exists(save_dir)) {
-    dir.create(save_dir)
+  if (config$run == "test") {
+    data_path = config$datapath$test
   }
-  time <- Sys.time()
-  save_dir = file.path(save_dir, gsub(' ', '', gsub('-', '', gsub(':', '', time))))
-  if (!dir.exists(save_dir)) {
-    dir.create(save_dir)
-  }
-  train_save_path = file.path(save_dir, "train_activation_probas.csv")
-  save_path = file.path(save_dir, "activation_probas.csv")
-} else {
-  save_path = NULL
-}
-
-predict_proba_wrapper = function(train_data, test_data, config, fit_model, next_proba) {
-  # Find best model on train set
-  best_model = model_selection(train_data, config$model.params, fit_model)
-
-  probas = predict_proba(train_data, test_data, config$window_size, best_model$model,
-                         fit_model, next_proba)
-
-  return(probas)
-}
-
-t1 = Sys.time()
-counter = 1
-for (cv in 1:length(config$data_specs)) {
-  print(cv)
-  cv_save_dir = file.path(save_dir, (cv - 1))
-  if (!dir.exists(cv_save_dir)) {
-    dir.create(cv_save_dir)
-  }
-  # load dataset
-  data_spec = config$data_specs[[cv]]
-  if (run == "train") {
-    test_start = data_spec$val_start
-    end = data_spec$end
-  }
-  if (run == "val") {
-    test_start = data_spec$val_start
-    end = data_spec$test_start
-  }
-  if (run == "test") {
-    test_start = data_spec$test_start
-    end = data_spec$end
-  }
-  data = get_cv_data(data_path,
-                     data_spec$start,
-                     test_start,
-                     end,
-                     config$window_size)
-  print(paste0("Last train: ", index(tail(data$train, 1))[1]))
-  print(paste0("First test: ", index(data$test)[1]))
-
-  factors = colnames(data$train)
-  # Initialize tables
-  if (counter == 1) {
-    activation_probas = data.frame(matrix(ncol = length(factors), nrow = 0))
-    train_activation_probas = data.frame(matrix(ncol = length(factors), nrow = 0))
-  }
-  cv_train_activation_probas = setNames(data.frame(matrix(ncol = length(factors),
-                                                          nrow = nrow(data$train))),
-                                        factors)
-  cv_activation_probas = setNames(data.frame(matrix(ncol = length(factors),
-                                                    nrow = nrow(data$test))),
-                                  factors)
-  for (ind in 1:length(factors)) {
-    # if (ind > 1){
-    #   break
-    # }
-    factor.name = factors[ind]
-    print(factor.name)
-    train_data = data$train[, ind]
-    test_data = data$test[, ind]
-
-    best_model = model_selection(train_data, config$model.params, fit_model, parallel = !debug)
-    model_path = file.path(cv_save_dir, paste0(factors[ind], "model.rds"))
-    saveRDS(best_model$model, file = model_path)
-
-    # Get proba on train set
-
-    if (!is.null(best_model$model)) {
-      dist_func = get_dist_functon(best_model$model@fit$params$cond.dist)
-      n.fitted = length(best_model$model@fitted)
-      train_probas = unname(sapply(-best_model$model@fitted / best_model$model@sigma.t, dist_func))
-      nans = nrow(train_data) - length(train_probas)
-      if (nans > 0) {
-        train_probas = c(rep(NaN, nans), train_probas) # c(rep(NaN, nans), as.vector(train_probas[,1]))
+  
+  counter = 1
+  for (cv in (length(config$data_specs)-2):length(config$data_specs)) {
+    print(cv)
+    if (save) {
+      cv_save_dir = file.path(save_dir, (cv - 1))
+      if (!dir.exists(cv_save_dir)) {
+        dir.create(cv_save_dir)
       }
-      probas = predict_proba(train_data, test_data, config$window_size, best_model$model,
-                             fit_model, next_proba, parallel = !debug)
-
-    } else {
-      train_probas = rep(NaN, length(index(train_data)))
-      probas = rep(NaN, length(index(test_data)))
     }
-    train_probas = xts(train_probas, order.by = index(train_data))
-    colnames(train_probas) = factor.name
-    probas = xts(probas, order.by = index(test_data))
-    colnames(probas) = factor.name
-
-    cv_train_activation_probas[factor.name] = train_probas
-    cv_activation_probas[factor.name] = probas
+    # load dataset
+    data_spec = config$data_specs[[cv]]
+    if (config$run == "train") {
+      test_start = data_spec$val_start
+      end = data_spec$end
+    }
+    if (config$run == "val") {
+      stop("Not implemented")
+      test_start = data_spec$val_start
+      end = data_spec$test_start
+    }
+    if (config$run == "test") {
+      test_start = data_spec$test_start
+      end = data_spec$end
+    }
+    data = get_cv_data(data_path,
+                       data_spec$start,
+                       test_start,
+                       end,
+                       config$window_size)
+    print(paste0("Last train: ", index(tail(data$train, 1))[1]))
+    print(paste0("First test: ", index(data$test)[1]))
+    
+    factors = colnames(data$train)
+    # Initialize tables
+    if (counter == 1) {
+      activation_probas = data.frame(matrix(ncol = length(factors), nrow = 0))
+      train_activation_probas = data.frame(matrix(ncol = length(factors), nrow = 0))
+    }
+    cv_train_activation_probas = setNames(data.frame(matrix(ncol = length(factors),
+                                                            nrow = nrow(data$train))),
+                                          factors)
+    cv_activation_probas = setNames(data.frame(matrix(ncol = length(factors),
+                                                      nrow = nrow(data$test))),
+                                    factors)
+    # Fit a ARMA-GARCH model and predict probas oos for each factor
+    for (ind in 1:length(factors)) {
+      # if (ind > 1){
+      #   break
+      # }
+      factor.name = factors[ind]
+      print(factor.name)
+      train_data = data$train[, ind]
+      test_data = data$test[, ind]
+      
+      # Model selection
+      best_model = model_selection(train_data, config$model.params, fit_model, parallel = !debug)
+      # Now get probas
+      if (!is.null(best_model$model)) {
+        # First get proba on train set for proba threshold tuning
+        dist_func = get_dist_functon(best_model$model@fit$params$cond.dist)
+        n.fitted = length(best_model$model@fitted)
+        train_probas = unname(sapply(-best_model$model@fitted / best_model$model@sigma.t, dist_func))
+        nans = nrow(train_data) - length(train_probas)
+        if (nans > 0) {
+          train_probas = c(rep(NaN, nans), train_probas) # c(rep(NaN, nans), as.vector(train_probas[,1]))
+        }
+        # Now predict probas on test set
+        probas = predict_proba(train_data, test_data, config$window_size, best_model$model,
+                               fit_model, next_proba, parallel = !debug)
+      } else {
+        train_probas = rep(NaN, length(index(train_data)))
+        probas = rep(NaN, length(index(test_data)))
+      }
+      # Update dataframes
+      train_probas = xts(train_probas, order.by = index(train_data))
+      colnames(train_probas) = factor.name
+      probas = xts(probas, order.by = index(test_data))
+      colnames(probas) = factor.name
+      cv_train_activation_probas[factor.name] = train_probas
+      cv_activation_probas[factor.name] = probas
+    }
+    cv_train_activation_probas = xts(cv_train_activation_probas, order.by = index(train_data))
+    cv_activation_probas = xts(cv_activation_probas, order.by = index(test_data))
+    
+    if (save){
+      model_path = file.path(cv_save_dir, paste0(factors[ind], "model.rds"))
+      saveRDS(best_model$model, file = model_path)
+      write.zoo(cv_train_activation_probas,
+                file = file.path(cv_save_dir, "train_activation_probas.csv"),
+                sep = ",")
+      write.zoo(cv_activation_probas,
+                file = file.path(cv_save_dir, "activation_probas.csv"),
+                sep = ",")
+    }
+    
+    # Update with only the latest train dates (last month)
+    if (nrow(train_activation_probas) > 0) {
+      cv_train_activation_probas = cv_train_activation_probas[index(cv_train_activation_probas) > last_train_date,]
+    }
+    
+    train_activation_probas = rbind(train_activation_probas, cv_train_activation_probas)
+    train_activation_probas = as.xts(train_activation_probas)
+    activation_probas = rbind(activation_probas, cv_activation_probas)
+    activation_probas = as.xts(activation_probas)
+    counter = counter + 1
+    last_train_date = index(data$train)[nrow(data$train)]
+    
+    print("Train probas")
+    print(tail(train_activation_probas))
+    print("Test probas")
+    print(tail(activation_probas))
+    print("NaNs CV:")
+    print(paste("Train:", sum(is.na(cv_train_activation_probas))))
+    print(paste("Test:", sum(is.na(cv_activation_probas))))
+    
   }
-  cv_train_activation_probas = xts(cv_train_activation_probas, order.by = index(train_data))
-  cv_activation_probas = xts(cv_activation_probas, order.by = index(test_data))
-  write.zoo(cv_train_activation_probas,
-            file = file.path(cv_save_dir, "train_activation_probas.csv"),
-            sep = ",")
-  write.zoo(cv_activation_probas,
-            file = file.path(cv_save_dir, "activation_probas.csv"),
-            sep = ",")
-
-  if (nrow(train_activation_probas) > 0) {
-    cv_train_activation_probas = cv_train_activation_probas[index(cv_train_activation_probas) > last_train_date,]
-  }
-
-  train_activation_probas = rbind(train_activation_probas, cv_train_activation_probas)
-  train_activation_probas = as.xts(train_activation_probas)
-  activation_probas = rbind(activation_probas, cv_activation_probas)
-  activation_probas = as.xts(activation_probas)
-
-  print("Train probas")
-  print(tail(train_activation_probas))
-  print("Test probas")
-  print(tail(activation_probas))
-  counter = counter + 1
-  last_train_date = index(data$train)[nrow(data$train)]
-
-}
-t2 = Sys.time()
-print(paste("Total time:", t2 - t1))
-
-if (save) {
-  write.zoo(train_activation_probas, file = train_save_path, sep = ",")
-  write.zoo(activation_probas, file = save_path, sep = ",")
+  return (list(train=train_activation_probas, test=activation_probas))
 }
--- tail_events/tail_events.R
+++ tail_events/tail_events.R
@@ -1,142 +1,53 @@
 rm(list = ls(all = TRUE))
 graphics.off()
+source("run.R")
+library(rjson)
 
-# library("rjson")
-source("utils.R")
-
-TEST = TRUE
-set_threshold=TRUE
-dir_ = "prediction_w4months_q1%_q5%_q2%_q10%"
-
-# ECON parameters
-# save dir
-save_dir = "./saved_models_Haindorf2020_results/"
-
-# Respect order !!
-qs = c(0.01, 0.025, 0.05, 0.1)
-window_size = 4 * MONTH # Fit model on 4 months history
-q_fit = 0.05  # fit to 10% of worst outcomes
-
-set.seed(7)
-
-dset.name  = paste(save_dir, dir_, "/dfdata.csv", sep = "")
-dataset <- read.csv(dset.name, header = TRUE)
-
-dates = dataset$X
-rownames(dataset) = dates
-dataset = dataset[ , -which(names(dataset) %in% c("returns","X"))]
-dataset <- timeSeries::as.timeSeries(dataset, FinCenter = "GMT")
-
-# Convert price series to losses and lower (negative) threshold to a positive threshold
-dataset = cbind(dataset, c(NaN, - diff(log(dataset$close))))
-colnames(dataset) = c(colnames(dataset)[1:ncol(dataset)-1], "loss")
-head(dataset)
+# ------------------------ Input ------------------------
+# Input
+dataset = "dataset2"
+config_path = "./config/dataset2.json"
+save = TRUE
+save_dir = "output"
+do.debug = FALSE
 
 
-dataset[, lower_cols] = - dataset[, lower_cols]
+# ------------------------ Script ------------------------
 
-# Start just before validation set
-dataset = dataset[rownames(dataset) >= '2017-06-01 00:00:00', ]
-length.dataset = nrow(dataset)
-dates = rownames(dataset)
-# Split data
-test_size = length.dataset - window_size
-prediction = matrix(nrow = test_size, ncol = (3 + (11 * length(qs))))
-
-if (TEST){
-    test_size = 20
-}
-
-count = 1
-time <- Sys.time()
-save_path = gsub(' ', '', gsub('-', '', gsub(':', '', time)))
-
-save_path = paste0(save_dir, dir_, '/', save_path, sep="")
-print(save_path)
-
-########################
-
-for (i in (window_size + 1):(window_size + test_size)) {
-    if ((test_size + window_size - i) %% 100 == 0){
-        print(paste0("Steps to go: ", test_size + window_size - i))
-    }
-    if (i %% 1000 == 0) {
-        print(paste("Saving prediction at step", i))
-        write.csv(prediction, paste0(save_path, "prediction_online_first.csv"), row.names = FALSE)
-    }
-    data.series = dataset[(i - window_size):(i - 1), "loss"]
-    # Normalize entire dataset to have variance one
-    return.sd = apply(data.series, 2, sd)
-    data.series = data.series$loss / return.sd
-    next.return = dataset[i, "loss"]
-    lowers = dataset[i - 1, lower_cols]/ return.sd
-    date = dates[i]
-
-    # Fit model and get prediction
-    if (!TEST){
-      prediction_i =   tryCatch(
-        fit_predParallel(date, data.series, next.return, return.sd, lowers, n=window_size, q_fit=q_fit,
-                         qs=qs),
-        error = function(e)
-          base::rep(NA, ncol(prediction)), 
-        silent = FALSE)
-    }
-    if (TEST){
-      prediction_i =  fit_predParallel(date, data.series, next.return, return.sd, lowers, n=window_size, q_fit=q_fit,
-                     qs=qs)
-    }
-    
-    prediction[count,] = as.vector(prediction_i)
-    count = count + 1
-}
-
-data_pred = prediction[!is.na(prediction[, 1]),]
-dates = data_pred[, 1]
-data_pred = data_pred[,-1]
-mode(data_pred) = "double"
-df = data.frame(data_pred)
-
-
-cn = c()
-for (q in qs){
-    cn = c(cn,
-    paste0("lower_", q),
-    paste0("threshold_", q),
-    paste0("var_evt_", q),
-    paste0("es_evt_", q),
-    paste0("proba_evt_", q),
-    paste0("var_norm_", q),
-    paste0("es_norm_", q),
-    paste0("proba_norm_", q),
-    paste0("mean_", q),
-    paste0("sd_", q),
-    paste0("zq_", q)
-    )
-}
-
-colnames(df) = c(
-"std_losses",
-"norm_sd",
-cn
-)
-rownames(df) = dates
-
-######### SAVE
-if (TEST){
-    write.csv(df, paste0(
-    paste0('./',
-    save_path),
-    "_prediction_10per_proba_TEST.csv"
-    ),
-    row.names = TRUE)
+config = fromJSON(file = config_path)
+if (save) {
+  cwd = getwd()
+  save_dir = file.path(cwd, save_dir)
+  if (!dir.exists(save_dir)) {
+    dir.create(save_dir)
+  }
+  save_dir = file.path(save_dir, dataset)
+  if (!dir.exists(save_dir)) {
+    dir.create(save_dir)
+  }
+  save_dir = file.path(save_dir, config$run)
+  if (!dir.exists(save_dir)) {
+    dir.create(save_dir)
+  }
+  time <- Sys.time()
+  save_dir = file.path(save_dir, gsub(' ', '', gsub('-', '', gsub(':', '', time))))
+  if (!dir.exists(save_dir)) {
+    dir.create(save_dir)
+  }
+  train_save_path = file.path(save_dir, "train_activation_probas.csv")
+  save_path = file.path(save_dir, "activation_probas.csv")
 } else {
-    write.csv(df,
-    paste0(save_path,
-    "_multi_q_prediction_qfit_",
-    substr(as.character(q_fit),
-    3,
-    4),
-    ".csv"),
-    row.names = TRUE
-    )
+  save_path = NULL
+}
+
+# Main loop
+t1 = Sys.time()
+result = run(config, save_dir = save_dir, debug = do.debug)
+t2 = Sys.time()
+print(paste("Total time:", t2 - t1))
+
+# Finally
+if (save) {
+  write.zoo(result$train, file = train_save_path, sep = ",")
+  write.zoo(result$test, file = save_path, sep = ",")
 }
