dda15d7e94c057beec9bf968d8a440ed775a0192
BrunoSpilak <bruno.spilak@gmail.com>
2022-03-21 16:20:01 +0100

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dl_portfolio',
-    version='1.3.1',
+    version='2.0.0',
     url='https://github.com/BrunoSpilak/dl-portfolio.git',
     author='Bruno Spilak',
     author_email='bruno.spilak@gmail.com',
