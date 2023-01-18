f1c2a54a304248ee1866de34526e4421f5ddc39c
BrunoSpilak <bruno.spilak@gmail.com>
2023-01-18 13:34:12 +0100

    reformat

--- dl_portfolio/cluster.py
+++ dl_portfolio/cluster.py
@@ -10,7 +10,11 @@ from fastcluster import linkage
 
 def get_cluster_assignment(base_dir, cluster_names):
     models = os.listdir(base_dir)
-    paths = [f"{base_dir}/{d}" for d in models if os.path.isdir(f"{base_dir}/{d}") and d[0] != "."]
+    paths = [
+        f"{base_dir}/{d}"
+        for d in models
+        if os.path.isdir(f"{base_dir}/{d}") and d[0] != "."
+    ]
     n_folds = os.listdir(paths[0])
     n_folds = sum([d.isdigit() for d in n_folds])
 
@@ -19,15 +23,22 @@ def get_cluster_assignment(base_dir, cluster_names):
     for cv in range(n_folds):
         cv_labels[cv] = {}
         for i, path in enumerate(paths):
-            embedding = pd.read_pickle(f'{path}/{cv}/encoder_weights.p')
+            embedding = pd.read_pickle(f"{path}/{cv}/encoder_weights.p")
             c, cv_labels[cv][i] = get_cluster_labels(embedding)
 
-        cons_mat = consensus_matrix(cv_labels[cv], reorder=True, method="single")
-        cluster_assignment[cv] = assign_cluster_from_consmat(cons_mat, cluster_names, t=0)
+        cons_mat = consensus_matrix(
+            cv_labels[cv], reorder=True, method="single"
+        )
+        cluster_assignment[cv] = assign_cluster_from_consmat(
+            cons_mat, cluster_names, t=0
+        )
 
     return cluster_assignment
 
-def assign_cluster_from_consmat(cons_mat: pd.DataFrame, cluster_names: List[str], t: float):
+
+def assign_cluster_from_consmat(
+    cons_mat: pd.DataFrame, cluster_names: List[str], t: float
+):
     """
     First check if asset fall in one (or more) of the clusters for a certain proportion of runs (t)
     Then, select cluster which includes the asset the most
@@ -38,7 +49,9 @@ def assign_cluster_from_consmat(cons_mat: pd.DataFrame, cluster_names: List[str]
     """
     assigned = cons_mat.loc[cluster_names].sum() > t
     cluster_assignment = pd.Series(index=cons_mat.index, dtype="object")
-    cluster_assignment.loc[assigned] = cons_mat.loc[cluster_names, assigned].idxmax()
+    cluster_assignment.loc[assigned] = cons_mat.loc[
+        cluster_names, assigned
+    ].idxmax()
     # reorder index
     cluster_assignment = cluster_assignment.loc[cons_mat.index]
     return cluster_assignment
@@ -47,51 +60,70 @@ def assign_cluster_from_consmat(cons_mat: pd.DataFrame, cluster_names: List[str]
 def get_cluster_labels(embedding: pd.DataFrame, threshold: float = 0.1):
     encoding_dim = embedding.shape[-1]
     if threshold is None:
-        kmeans = KMeans(n_clusters=encoding_dim, random_state=0).fit(embedding.values)
-        labels = pd.DataFrame(kmeans.labels_, index=embedding.index, columns=['label'])
+        kmeans = KMeans(n_clusters=encoding_dim, random_state=0).fit(
+            embedding.values
+        )
+        labels = pd.DataFrame(
+            kmeans.labels_, index=embedding.index, columns=["label"]
+        )
     else:
         mask = embedding >= threshold
-        labels = pd.DataFrame(columns=['label'], index=mask.index)
+        labels = pd.DataFrame(columns=["label"], index=mask.index)
         for c in range(encoding_dim):
-            labels.loc[mask.index[mask[c]], 'label'] = c
+            labels.loc[mask.index[mask[c]], "label"] = c
 
         # When orthogonality constraint is not met, take max
-        multi_c = (labels['label'] != embedding.idxmax(1)) & (~labels['label'].isna())
-        labels.loc[multi_c, 'label'] = embedding.idxmax(1).loc[multi_c]
+        multi_c = (labels["label"] != embedding.idxmax(1)) & (
+            ~labels["label"].isna()
+        )
+        labels.loc[multi_c, "label"] = embedding.idxmax(1).loc[multi_c]
         labels = labels.fillna(
-            encoding_dim)  # Some assets might not be assigned to any cluster, put them in another cluster
+            encoding_dim
+        )  # Some assets might not be assigned to any
+        # cluster, put them in another cluster
 
-    cluster_labels = np.unique(labels['label'].values).tolist()
+    cluster_labels = np.unique(labels["label"].values).tolist()
     cluster_labels.sort()
 
     clusters = {}
     for c in cluster_labels:
-        assets = list(labels.loc[labels['label'] == c].index)
+        assets = list(labels.loc[labels["label"] == c].index)
         clusters[c] = assets
 
-    clusters = {i: clusters.get(i) for i in range(len(cluster_labels)) if clusters.get(i) is not None}  # reorder dict
+    clusters = {
+        i: clusters.get(i)
+        for i in range(len(cluster_labels))
+        if clusters.get(i) is not None
+    }  # reorder dict
 
     return clusters, labels
 
 
-def consensus_matrix(labels: Dict, reorder=False, method='single'):
+def consensus_matrix(labels: Dict, reorder=False, method="single"):
     """
 
-    :param cv_labels: Dictionnary with following structure {run_i: {}, ...}
+    :param method:
+    :param reorder:
+    :param labels:
     :return:
     """
     n_runs = len(labels)
-    assets = labels[0]['label'].index
+    assets = labels[0]["label"].index
     n_assets = len(assets)
 
     connect_mat = []
     for i in range(n_runs):
-        cm = pd.DataFrame(np.eye(n_assets), columns=assets, index=assets, dtype=np.uint8)
-        classes = labels[i]['label'].unique().tolist()
+        cm = pd.DataFrame(
+            np.eye(n_assets), columns=assets, index=assets, dtype=np.uint8
+        )
+        classes = labels[i]["label"].unique().tolist()
 
         for label in classes:
-            cluster = list(assets[labels[i]['label'] == label])
-            cluster = {a: [x for j, x in enumerate(cluster) if i != j] for i, a in enumerate(cluster)}
+            cluster = list(assets[labels[i]["label"] == label])
+            cluster = {
+                a: [x for j, x in enumerate(cluster) if i != j]
+                for i, a in enumerate(cluster)
+            }
             for c in cluster:
                 cm.loc[c, cluster[c]] = 1
 
@@ -103,9 +135,13 @@ def consensus_matrix(labels: Dict, reorder=False, method='single'):
     cons_mat = cons_mat / n_runs
 
     if reorder:
-        cons_mat, res_order, res_linkage = compute_serial_matrix((1 - cons_mat).values, method)
+        cons_mat, res_order, res_linkage = compute_serial_matrix(
+            (1 - cons_mat).values, method
+        )
         new_order = [assets[i] for i in res_order]
-        cons_mat = pd.DataFrame(1 - cons_mat, index=new_order, columns=new_order)
+        cons_mat = pd.DataFrame(
+            1 - cons_mat, index=new_order, columns=new_order
+        )
 
     return cons_mat
 
@@ -120,7 +156,9 @@ def rand_score_permutation(labels):
     rand = np.zeros((n_runs, n_runs))
     for i in range(n_runs):
         for j in range(n_runs):
-            rand[i, j] = metrics.adjusted_rand_score(labels[i]['label'], labels[j]['label'])
+            rand[i, j] = metrics.adjusted_rand_score(
+                labels[i]["label"], labels[j]["label"]
+            )
     return rand
 
 
@@ -141,7 +179,7 @@ def seriation(Z, N, cur_index):
     else:
         left = int(Z[cur_index - N, 0])
         right = int(Z[cur_index - N, 1])
-        return (seriation(Z, N, left) + seriation(Z, N, right))
+        return seriation(Z, N, left) + seriation(Z, N, right)
 
 
 def compute_serial_matrix(dist_mat, method="ward"):
@@ -169,7 +207,9 @@ def compute_serial_matrix(dist_mat, method="ward"):
     res_order = seriation(res_linkage, N, N + N - 2)
     seriated_dist = np.zeros((N, N))
     a, b = np.triu_indices(N, k=1)
-    seriated_dist[a, b] = dist_mat[[res_order[i] for i in a], [res_order[j] for j in b]]
+    seriated_dist[a, b] = dist_mat[
+        [res_order[i] for i in a], [res_order[j] for j in b]
+    ]
     seriated_dist[b, a] = seriated_dist[a, b]
 
     return seriated_dist, res_order, res_linkage
