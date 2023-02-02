aa32f749a624abd41d7009e42886d7cc69a49e46
BrunoSpilak <bruno.spilak@gmail.com>
2023-02-02 12:26:25 +0100

    cleaning

--- prado/monte_carlo.py
+++ prado/monte_carlo.py
@@ -2,37 +2,29 @@
 
 # On 20151231 by MLdP <lopezdeprado@lbl.gov>
 import os
-import pdb
-import time
-
-import fastcluster
 import scipy.cluster.hierarchy as sch
 import random
 import numpy as np
 import pandas as pd
 from joblib import Parallel, delayed
-import tensorflow as tf
-from matplotlib import pyplot as plt
-from scipy.cluster import hierarchy
-from statsmodels.stats.correlation_tools import corr_nearest
-
-import seaborn as sns
 
 import prado.cla.CLA as CLA
+from dl_portfolio.cluster import get_cluster_labels
 from dl_portfolio.logger import LOGGER
 from dl_portfolio.nmf.convex_nmf import ConvexNMF
-from dl_portfolio.weights import ae_riskparity_weights
+from dl_portfolio.weights import ae_riskparity_weights, \
+    get_inner_cluster_weights
 from prado.hrp import correlDist, getIVP, getQuasiDiag, getRecBipart
 
 import json
 
-# ------------------------------------------------------------------------------
 
 def dgp_mapper(dgp_name, dgp_params):
     if dgp_name == "hrp_mc":
         return generate_data_hrp(**dgp_params)
     else:
         raise NotImplementedError(dgp_name)
+
 
 def generate_data_hrp(n_obs, sLength, size0, size1, mu0, sigma0, sigma1F):
     # Time series of correlated variables
@@ -117,15 +109,24 @@ def getNMF(train_data, n_components, market_budget, method="ae_rp_c"):
     loading = pd.DataFrame(model.components.copy())
 
     if method == "ae_rp_c":
-        weights = ae_riskparity_weights(pd.DataFrame(train_data),
+        returns = pd.DataFrame(train_data)
+        weights = ae_riskparity_weights(returns,
                                         embedding, loading,
                                         market_budget=market_budget,
                                         risk_parity="cluster",
                                         threshold=0.)
+
+        max_cluster = embedding.shape[-1] - 1
+        # First get cluster allocation to forget about small contribution
+        clusters, _ = get_cluster_labels(embedding, threshold=0.)
+        clusters = {c: clusters[c] for c in clusters if c <= max_cluster}
+        inner_weights = get_inner_cluster_weights(returns.cov(), loading,
+                                                  clusters, market_budget)
+        inner_weights = pd.DataFrame(inner_weights)
     else:
         raise NotImplementedError()
 
-    return weights
+    return weights, inner_weights
 
 
 def worker(steps, methods_mapper, dgp_name=None, dgp_params=None, sLength=260,
@@ -142,6 +143,7 @@ def worker(steps, methods_mapper, dgp_name=None, dgp_params=None, sLength=260,
 
     r = {i: pd.Series(dtype=np.float32) for i in methods_mapper}
     weights = {i: pd.DataFrame() for i in methods_mapper}
+    inner_weights = pd.DataFrame()
 
     pointers = range(sLength, len(returns), rebal)
 
@@ -157,14 +159,15 @@ def worker(steps, methods_mapper, dgp_name=None, dgp_params=None, sLength=260,
         x_ = returns[pointer:pointer + rebal]
         for func_name in methods_mapper:
             if func_name == "NMF":
-                w_ = methods_mapper[func_name](in_x_, n_components,
-                                               market_budget=market_budget)
+                w_, in_w_ = methods_mapper[func_name](in_x_, n_components,
+                                                      market_budget=market_budget)
+                inner_weights = pd.concat([inner_weights, in_w_])
             else:
                 assert func_name in ["IVP", "HRP"]
                 w_ = methods_mapper[func_name](cov=cov_, corr=corr_)
             r_ = pd.Series(np.dot(x_, w_))
             r[func_name] = r[func_name].append(r_)
-            weights[func_name] = pd.concat([weights[func_name], w_], axis=0)
+            weights[func_name] = pd.concat([weights[func_name], w_])
 
     # 4) Evaluate and store results
     port_returns = {}
@@ -175,7 +178,7 @@ def worker(steps, methods_mapper, dgp_name=None, dgp_params=None, sLength=260,
 
         port_returns[func_name] = r_
 
-    return port_returns, weights, cluster_mapper  # , stats
+    return port_returns, weights, cluster_mapper, inner_weights
 
 
 def mc_hrp(methods_mapper,  dgp_name,  dgp_params, n_jobs=1,
@@ -201,6 +204,7 @@ def mc_hrp(methods_mapper,  dgp_name,  dgp_params, n_jobs=1,
         #         func_name]
 
         clusters = []
+        inner_weights = []
         for numIter in range(num_iters):
             for func_name in methods_mapper:
                 returns[func_name] = pd.concat(
@@ -218,11 +222,14 @@ def mc_hrp(methods_mapper,  dgp_name,  dgp_params, n_jobs=1,
                     axis=1
                 )
             clusters.append(results[numIter][2])
+            inner_weights.append(results[numIter][3])
 
     else:
         clusters = []
+        inner_weights = []
         for numIter in range(num_iters):
-            returns_iter, weights_iter, cluster_iter = worker(
+            returns_iter, weights_iter, cluster_iter, inner_weights_iter = \
+                worker(
                 num_iters - numIter, methods_mapper, dgp_name, dgp_params,
                 sLength=sLength, rebal=rebal)
             clusters.append(cluster_iter)
@@ -243,6 +250,8 @@ def mc_hrp(methods_mapper,  dgp_name,  dgp_params, n_jobs=1,
                     ],
                     axis=1
                 )
+            inner_weights.append(
+                inner_weights_iter.reset_index().to_json(orient="index"))
 
     for func_name in methods_mapper:
         returns[func_name].columns = list(range(num_iters))
@@ -258,102 +267,7 @@ def mc_hrp(methods_mapper,  dgp_name,  dgp_params, n_jobs=1,
         returns[func_name].to_csv(f"{save_dir}/returns_{func_name}.csv")
         weights[func_name].to_csv(f"{save_dir}/weights_{func_name}.csv")
 
-    json.dump(clusters, open(f"{save_dir}/clusters.json", "w"))
-    return
-
-
-def mc_from_corr(methods_mapper, batch, n_jobs=8,
-                 num_iters=int(1e4), sLength=260, rebal=22,  save_dir=None):
-    assert os.path.isdir(save_dir)
-    generator = tf.keras.models.load_model(
-        "prado/corrgan-models/saved_model/generator_100d")
-
-    # Monte Carlo experiment on HRP
-    # stats = {i: pd.Series() for i in methods_mapper}
-    returns = {k: pd.DataFrame() for k in methods_mapper}
-    weights = {k: pd.DataFrame() for k in methods_mapper}
-
-    counter = 0
-    if n_jobs > 1:
-        with Parallel(n_jobs=n_jobs) as _parallel_pool:
-            results = _parallel_pool(
-                    delayed(worker)(
-                        num_iters - counter, methods_mapper, dgp_name=None,
-                        dgp_params=None, sLength=sLength, rebal=rebal,
-                        returns=batch[numIter][0], col_cluster_mapper=batch[
-                            numIter][1],
-                        cluster_mapper=batch[numIter][2]
-                    )
-                    for numIter in range(num_iters)
-            )
-        # for numIter in range(numIters):
-        #     for func_name in methods_mapper:
-        #         stats[func_name].loc[numIter] = stats_iter[numIter][
-        #         func_name]
-
-        clusters = []
-        for numIter in range(num_iters):
-            for func_name in methods_mapper:
-                returns[func_name] = pd.concat(
-                    [
-                        returns[func_name],
-                        results[numIter][0][func_name]
-                    ],
-                    axis=1
-                )
-                weights[func_name] = pd.concat(
-                    [
-                        weights[func_name],
-                        results[numIter][1][func_name]
-                    ],
-                    axis=1
-                )
-            clusters.append(results[numIter][2])
-    else:
-        clusters = []
-        for numIter in range(num_iters):
-            returns_iter, weights_iter, cluster_iter = worker(
-                num_iters - counter, methods_mapper, dgp_name=None,
-                dgp_params=None, sLength=sLength, rebal=rebal,
-                returns=batch[numIter][0], col_cluster_mapper=batch[
-                    numIter][1],
-                cluster_mapper=batch[numIter][2]
-            )
-            clusters.append(cluster_iter)
-            # for func_name in methods_mapper:
-            #     stats[func_name].loc[numIter] = stats_iter[func_name]
-            for func_name in methods_mapper:
-                returns[func_name] = pd.concat(
-                    [
-                        returns[func_name],
-                        returns_iter[func_name]
-                    ],
-                    axis=1
-                )
-                weights[func_name] = pd.concat(
-                    [
-                        weights[func_name],
-                        weights_iter[func_name]
-                    ],
-                    axis=1
-                )
-
-
-
-    for func_name in methods_mapper:
-        returns[func_name].columns = list(range(num_iters))
-        weights[func_name].columns = list(range(num_iters))
-
-    # 5) Report results
-    # stats = pd.DataFrame.from_dict(returns, orient='columns')
-    # stats.to_csv('stats.csv')
-    # df0, df1 = stats.std(), stats.var()
-    # print(stats)
-    # print(pd.concat([df0, df1, df1 / df1['getHRP'] - 1], axis=1))
-    for func_name in methods_mapper:
-        returns[func_name].to_csv(f"{save_dir}/returns_{func_name}.csv")
-        weights[func_name].to_csv(f"{save_dir}/weights_{func_name}.csv")
-
+    json.dump(inner_weights, open(f"{save_dir}/inner_weights_NMF.json", "w"))
     json.dump(clusters, open(f"{save_dir}/clusters.json", "w"))
     return
 
@@ -369,9 +283,6 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dgp",
-                        type=str,
-                        help="DGP name: hrp_mc or corrgan")
     parser.add_argument("--n_jobs",
                         default=os.cpu_count(),
                         type=int,
@@ -389,63 +300,15 @@ if __name__ == '__main__':
     if not os.path.isdir(save_dir):
         os.mkdir(save_dir)
 
-    if args.dgp == "hrp_mc":
-        dgp_params = {
-            "n_obs": 520,
-            "size0": 6,
-            "size1": 9,
-            "mu0": 0,
-            "sigma0": 1e-2,
-            "sigma1F": 0.25,
-            "sLength": 260,
-        }
-    elif args.dgp == "corrgan":
-        dgp_params = {
-            "n_assets": 100,
-            "n_obs": 520,
-            "min_sigma": 0.0025,
-            "max_sigma": 0.015,
-            "seed": 1234,
-            # "sLength": 260,
-        }
-    elif args.dgp == "from_corr":
-        raise NotImplementedError
-        # dgp_params = {
-        #     "n_assets": 100,
-        #     "n_obs": 520,
-        #     "min_sigma": 0.0025,
-        #     "max_sigma": 0.015,
-        #     "seed": 1234,
-        #     # "sLength": 260,
-        # }
-        # generator = tf.keras.models.load_model(
-        #     "prado/corrgan-models/saved_model/generator_100d")
-        # batch = generate_batch_corr_mat(10,
-        #                                 dgp_params["n_assets"],
-        #                                 generator=generator,
-        #                                 seed=None,
-        #                                 nearest=False)
-        # batch = [
-        #     [
-        #         generate_data_from_corr_mat(
-        #             dgp_params["n_assets"],
-        #             dgp_params["n_obs"],
-        #             batch[i][0],
-        #             np.random.uniform(dgp_params["min_sigma"],
-        #                               dgp_params["max_sigma"],
-        #                               dgp_params["n_assets"]).reshape(
-        #                 dgp_params["n_assets"], 1)
-        #         ),
-        #         batch[i][1],
-        #         batch[i][2],
-        #     ] for i in range(args.num_iters)
-        # ]
-        #
-        # mc_from_corr(methods_mapper, batch, n_jobs=1,
-        #              num_iters=args.num_iters, sLength=260, rebal=22,
-        #              save_dir=save_dir)
-    else:
-        raise NotImplementedError(args.dgp_name)
+    dgp_params = {
+        "n_obs": 520,
+        "size0": 6,
+        "size1": 9,
+        "mu0": 0,
+        "sigma0": 1e-2,
+        "sigma1F": 0.25,
+        "sLength": 260,
+    }
 
-    mc_hrp(METHODS_MAPPER, args.dgp,  dgp_params, n_jobs=args.n_jobs,
+    mc_hrp(METHODS_MAPPER, "hrp_mc",  dgp_params, n_jobs=args.n_jobs,
            num_iters=args.num_iters, save_dir=save_dir)
