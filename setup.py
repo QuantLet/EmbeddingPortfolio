0f24ad2699a712073dc3796d50cfd707086e3497
BrunoSpilak <bruno.spilak@gmail.com>
2023-02-02 12:26:20 +0100

    minor

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         "joblib==1.0.0",
         "scikit-learn==0.24.0",
         "fastcluster==1.2.6",
+        "statsmodels==0.13.5"
     ],
     zip_safe=False,
     packages=find_packages()
