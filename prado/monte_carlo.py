f8f825f2163edd3e182797d562a60c08c1f16292
BrunoSpilak <bruno.spilak@gmail.com>
2023-02-08 17:20:55 +0100

    minor

--- prado/monte_carlo.py
+++ prado/monte_carlo.py
@@ -12,8 +12,10 @@ import prado.cla.CLA as CLA
 from dl_portfolio.cluster import get_cluster_labels
 from dl_portfolio.logger import LOGGER
 from dl_portfolio.nmf.convex_nmf import ConvexNMF
-from dl_portfolio.weights import ae_riskparity_weights, \
-    get_inner_cluster_weights
+from dl_portfolio.weights import (
+    ae_riskparity_weights,
+    get_inner_cluster_weights,
+)
 from prado.hrp import correlDist, getIVP, getQuasiDiag, getRecBipart
 from arch import arch_model
 
@@ -36,24 +38,28 @@ def generate_data_hrp(n_obs, sLength, size0, size1, mu0, sigma0, sigma1F):
 
     # 2) create correlation between the variables
     cols = [random.randint(0, size0 - 1) for i in range(size1)]
-    y = x[:, cols] + np.random.normal(0, sigma0 * sigma1F,
-                                      size=(n_obs, len(cols)))
+    y = x[:, cols] + np.random.normal(
+        0, sigma0 * sigma1F, size=(n_obs, len(cols))
+    )
     x = np.append(x, y, axis=1)
     # 3) add common random shock
     point = np.random.randint(sLength, n_obs - 1, size=2)
-    x[np.ix_(point, [cols[0], size0])] = np.array([[-.5, -.5], [2, 2]])
+    x[np.ix_(point, [cols[0], size0])] = np.array([[-0.5, -0.5], [2, 2]])
 
     # 4) add specific random shock
     point = np.random.randint(sLength, n_obs - 1, size=2)
-    x[point, cols[-1]] = np.array([-.5, 2])
-    cluster_mapper, col_cluster_mapper = get_cluster_mapper(size0, x.shape[-1],
-                                                            cols)
+    x[point, cols[-1]] = np.array([-0.5, 2])
+    cluster_mapper, col_cluster_mapper = get_cluster_mapper(
+        size0, x.shape[-1], cols
+    )
 
     return x, col_cluster_mapper, cluster_mapper
 
 
-def generate_garch(assets = ["BTC", "US_B", "SPX_X", "EUR_FX", "GOLDS_C"],
-                   sigmas: list = [0.08, 0.003, 0.01, 0.005, 0.007]):
+def generate_garch(
+    assets=["BTC", "US_B", "SPX_X", "EUR_FX", "GOLDS_C"],
+    sigmas: list = [0.08, 0.003, 0.01, 0.005, 0.007],
+):
     """
     Fit a GARCH process to the selected assets and generate data. Rescaled
     the data to match the volatilities.
@@ -62,13 +68,15 @@ def generate_garch(assets = ["BTC", "US_B", "SPX_X", "EUR_FX", "GOLDS_C"],
     :param sigmas:
     :return:
     """
-    data = pd.read_csv("data/dataset1/dataset1.csv", index_col=0,
-                       parse_dates=True)
+    data = pd.read_csv(
+        "data/dataset1/dataset1.csv", index_col=0, parse_dates=True
+    )
     generated = pd.DataFrame()
     for i, c in enumerate(assets):
         # define model
-        model = arch_model(data[c], mean='Zero', vol='GARCH', p=1, q=1,
-                           rescale=True)
+        model = arch_model(
+            data[c], mean="Zero", vol="GARCH", p=1, q=1, rescale=True
+        )
         model_fit = model.fit()
         sim = model.simulate(params=model_fit.params, nobs=520) / 10
         generated = pd.concat([generated, sim["data"]], axis=1)
@@ -82,10 +90,13 @@ def generate_garch(assets = ["BTC", "US_B", "SPX_X", "EUR_FX", "GOLDS_C"],
 
 
 def generate_data_cluster(
-        n_obs, sLength, size1: int = 15, mu0: list = [0] * 5,
-        sigma0: list = [0.08, 0.003, 0.01, 0.005, 0.007],
-        sigma1F: float = 0.25,
-        process="garch"
+    n_obs,
+    sLength,
+    size1: int = 15,
+    mu0: list = [0] * 5,
+    sigma0: list = [0.08, 0.003, 0.01, 0.005, 0.007],
+    sigma1F: float = 0.25,
+    process="garch",
 ):
     # Time series of correlated variables
     # 1) generate random uncorrelated data
@@ -106,18 +117,20 @@ def generate_data_cluster(
 
     y = np.zeros((n_obs, size1))
     for i, c in enumerate(cols):
-        y[:, i] = x[:, c] + np.random.normal(0, sigma0[c] * sigma1F,
-                                             size=n_obs)
+        y[:, i] = x[:, c] + np.random.normal(
+            0, sigma0[c] * sigma1F, size=n_obs
+        )
     x = np.append(x, y, axis=1)
     # 3) add common random shock
     point = np.random.randint(sLength, n_obs - 1, size=2)
-    x[np.ix_(point, [cols[0], size0])] = np.array([[-.5, -.5], [2, 2]])
+    x[np.ix_(point, [cols[0], size0])] = np.array([[-0.5, -0.5], [2, 2]])
 
     # 4) add specific random shock
     point = np.random.randint(sLength, n_obs - 1, size=2)
-    x[point, cols[-1]] = np.array([-.5, 2])
-    cluster_mapper, col_cluster_mapper = get_cluster_mapper(size0, x.shape[-1],
-                                                            cols)
+    x[point, cols[-1]] = np.array([-0.5, 2])
+    cluster_mapper, col_cluster_mapper = get_cluster_mapper(
+        size0, x.shape[-1], cols
+    )
 
     return x, col_cluster_mapper, cluster_mapper
 
@@ -129,8 +142,8 @@ def get_cluster_mapper(size0, nvars, cols):
         col_cluster_mapper[c] = cols[i]
     col_cluster_mapper = pd.Series(col_cluster_mapper)
     cluster_mapper = {
-        c: col_cluster_mapper.index[col_cluster_mapper == c].tolist() for c
-        in cluster_labels
+        c: col_cluster_mapper.index[col_cluster_mapper == c].tolist()
+        for c in cluster_labels
     }
     return cluster_mapper, col_cluster_mapper
 
@@ -139,7 +152,7 @@ def getHRP(cov, corr):
     # Construct a hierarchical portfolio
     corr, cov = pd.DataFrame(corr), pd.DataFrame(cov)
     dist = correlDist(corr)
-    link = sch.linkage(dist, 'single')
+    link = sch.linkage(dist, "single")
     sortIx = getQuasiDiag(link)
     sortIx = corr.index[sortIx].tolist()  # recover labels
     hrp = getRecBipart(cov, sortIx)
@@ -163,15 +176,16 @@ def getCLA(cov, **kargs):
 def create_art_market_budget(col_cluster_mapper):
     market_budget = pd.DataFrame(col_cluster_mapper, columns=["market"])
     market_budget = market_budget.reset_index(drop=False)
-    market_budget["rc"] = 1.
+    market_budget["rc"] = 1.0
     market_budget.columns = ["assets", "market", "rc"]
     market_budget["assets"] = market_budget["assets"].astype(str)
     market_budget["market"] = market_budget["market"].astype(str)
     return market_budget
 
 
-def getNMF(train_data, n_components, market_budget, threshold,
-           method="ae_rp_c"):
+def getNMF(
+    train_data, n_components, market_budget, threshold, method="ae_rp_c"
+):
     model = ConvexNMF(
         n_components=n_components,
         random_state=None,
@@ -184,18 +198,22 @@ def getNMF(train_data, n_components, market_budget, threshold,
 
     if method == "ae_rp_c":
         returns = pd.DataFrame(train_data)
-        weights = ae_riskparity_weights(returns,
-                                        embedding, loading,
-                                        market_budget=market_budget,
-                                        risk_parity="cluster",
-                                        threshold=threshold)
+        weights = ae_riskparity_weights(
+            returns,
+            embedding,
+            loading,
+            market_budget=market_budget,
+            risk_parity="cluster",
+            threshold=threshold,
+        )
 
         max_cluster = embedding.shape[-1] - 1
         # First get cluster allocation to forget about small contribution
         clusters, _ = get_cluster_labels(embedding, threshold=threshold)
         clusters = {c: clusters[c] for c in clusters if c <= max_cluster}
-        inner_weights = get_inner_cluster_weights(returns.cov(), loading,
-                                                  clusters, market_budget)
+        inner_weights = get_inner_cluster_weights(
+            returns.cov(), loading, clusters, market_budget
+        )
         inner_weights = pd.DataFrame(inner_weights)
     else:
         raise NotImplementedError()
@@ -203,9 +221,18 @@ def getNMF(train_data, n_components, market_budget, threshold,
     return weights, inner_weights
 
 
-def worker(steps, methods_mapper, dgp_name=None, dgp_params=None, sLength=260,
-           rebal=22, returns=None, col_cluster_mapper=None,
-           cluster_mapper=None, vol_target: float = 0.05):
+def worker(
+    steps,
+    methods_mapper,
+    dgp_name=None,
+    dgp_params=None,
+    sLength=260,
+    rebal=22,
+    returns=None,
+    col_cluster_mapper=None,
+    cluster_mapper=None,
+    vol_target: float = 0.05,
+):
     if steps % 10 == 0:
         LOGGER.info(f"Steps to go: {steps}")
 
@@ -213,7 +240,8 @@ def worker(steps, methods_mapper, dgp_name=None, dgp_params=None, sLength=260,
     # 1) Prepare data for one experiment
     if dgp_name:
         returns, col_cluster_mapper, cluster_mapper = dgp_mapper(
-            dgp_name, dgp_params)
+            dgp_name, dgp_params
+        )
 
     r = {i: pd.Series(dtype=np.float32) for i in methods_mapper}
     leverage = {i: [] for i in methods_mapper}
@@ -227,18 +255,20 @@ def worker(steps, methods_mapper, dgp_name=None, dgp_params=None, sLength=260,
 
     # 2) Compute portfolios in-sample
     for pointer in pointers:
-        in_x_ = returns[pointer - sLength:pointer]
+        in_x_ = returns[pointer - sLength : pointer]
         cov_, corr_ = np.cov(in_x_, rowvar=0), np.corrcoef(in_x_, rowvar=0)
 
         # 3) Compute performance out-of-sample
-        x_ = returns[pointer:pointer + rebal]
+        x_ = returns[pointer : pointer + rebal]
         in_x_ = returns[:pointer]
         for func_name in methods_mapper:
             try:
                 if func_name == "NMF":
                     w_, in_w_ = methods_mapper[func_name](
-                        in_x_, n_components, market_budget=market_budget,
-                        threshold=1e-2
+                        in_x_,
+                        n_components,
+                        market_budget=market_budget,
+                        threshold=1e-2,
                     )
                     inner_weights = pd.concat([inner_weights, in_w_])
                 else:
@@ -250,8 +280,7 @@ def worker(steps, methods_mapper, dgp_name=None, dgp_params=None, sLength=260,
                 if vol_target is not None:
                     in_r_ = np.dot(in_x_, w_)
                     base_vol = np.max(
-                        (np.std(in_r_[-20:]),
-                         np.std(in_r_[-60:]))
+                        (np.std(in_r_[-20:]), np.std(in_r_[-60:]))
                     ) * np.sqrt(252)
                     assert not np.isinf(base_vol)
                     assert not np.isnan(base_vol)
@@ -280,8 +309,16 @@ def worker(steps, methods_mapper, dgp_name=None, dgp_params=None, sLength=260,
     return port_returns, weights, cluster_mapper, inner_weights, leverage
 
 
-def mc_hrp(methods_mapper,  dgp_name,  dgp_params, n_jobs=1,
-           num_iters=int(1e4), sLength=260, rebal=22,  save_dir=None):
+def mc_hrp(
+    methods_mapper,
+    dgp_name,
+    dgp_params,
+    n_jobs=1,
+    num_iters=int(1e4),
+    sLength=260,
+    rebal=22,
+    save_dir=None,
+):
     assert save_dir is not None
     # Monte Carlo experiment on HRP
     # stats = {i: pd.Series() for i in methods_mapper}
@@ -293,8 +330,12 @@ def mc_hrp(methods_mapper,  dgp_name,  dgp_params, n_jobs=1,
         with Parallel(n_jobs=n_jobs) as _parallel_pool:
             results = _parallel_pool(
                 delayed(worker)(
-                    num_iters - numIter, methods_mapper, dgp_name,
-                    dgp_params, sLength=sLength, rebal=rebal
+                    num_iters - numIter,
+                    methods_mapper,
+                    dgp_name,
+                    dgp_params,
+                    sLength=sLength,
+                    rebal=rebal,
                 )
                 for numIter in range(num_iters)
             )
@@ -307,53 +348,51 @@ def mc_hrp(methods_mapper,  dgp_name,  dgp_params, n_jobs=1,
         for numIter in range(num_iters):
             for func_name in methods_mapper:
                 returns[func_name] = pd.concat(
-                    [
-                        returns[func_name],
-                        results[numIter][0][func_name]
-                    ],
-                    axis=1
+                    [returns[func_name], results[numIter][0][func_name]],
+                    axis=1,
                 )
                 weights[func_name] = pd.concat(
-                    [
-                        weights[func_name],
-                        results[numIter][1][func_name]
-                    ],
-                    axis=1
+                    [weights[func_name], results[numIter][1][func_name]],
+                    axis=1,
                 )
                 leverage[func_name].append(results[numIter][4][func_name])
 
             clusters.append(results[numIter][2])
             inner_weights.append(
-                results[numIter][3].reset_index().to_json(orient="index"))
+                results[numIter][3].reset_index().to_json(orient="index")
+            )
 
     else:
         clusters = []
         inner_weights = []
         for numIter in range(num_iters):
-            returns_iter, weights_iter, cluster_iter, inner_weights_iter, _ = \
-                worker(
-                num_iters - numIter, methods_mapper, dgp_name, dgp_params,
-                sLength=sLength, rebal=rebal)
+            (
+                returns_iter,
+                weights_iter,
+                cluster_iter,
+                inner_weights_iter,
+                _,
+            ) = worker(
+                num_iters - numIter,
+                methods_mapper,
+                dgp_name,
+                dgp_params,
+                sLength=sLength,
+                rebal=rebal,
+            )
             clusters.append(cluster_iter)
             # for func_name in methods_mapper:
             #     stats[func_name].loc[numIter] = stats_iter[func_name]
             for func_name in methods_mapper:
                 returns[func_name] = pd.concat(
-                    [
-                        returns[func_name],
-                        returns_iter[func_name]
-                    ],
-                    axis=1
+                    [returns[func_name], returns_iter[func_name]], axis=1
                 )
                 weights[func_name] = pd.concat(
-                    [
-                        weights[func_name],
-                        weights_iter[func_name]
-                    ],
-                    axis=1
+                    [weights[func_name], weights_iter[func_name]], axis=1
                 )
             inner_weights.append(
-                inner_weights_iter.reset_index().to_json(orient="index"))
+                inner_weights_iter.reset_index().to_json(orient="index")
+            )
 
     for func_name in methods_mapper:
         returns[func_name].columns = list(range(num_iters))
@@ -375,33 +414,30 @@ def mc_hrp(methods_mapper,  dgp_name,  dgp_params, n_jobs=1,
     return
 
 
-METHODS_MAPPER = {
-    "IVP": getIVP_mc,
-    "HRP": getHRP,
-    "NMF": getNMF
-}
+METHODS_MAPPER = {"IVP": getIVP_mc, "HRP": getHRP, "NMF": getNMF}
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import datetime as dt
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dgp",
-                        default="hrp_mc",
-                        type=str,
-                        help="Data generating process")
-    parser.add_argument("--n_jobs",
-                        default=os.cpu_count(),
-                        type=int,
-                        help="Number of parallel jobs")
-    parser.add_argument("--num_iters",
-                        default=int(1e4),
-                        type=int,
-                        help="Number of MC iterations")
+    parser.add_argument(
+        "--dgp", default="hrp_mc", type=str, help="Data generating process"
+    )
+    parser.add_argument(
+        "--n_jobs",
+        default=os.cpu_count(),
+        type=int,
+        help="Number of parallel jobs",
+    )
+    parser.add_argument(
+        "--num_iters",
+        default=int(1e4),
+        type=int,
+        help="Number of MC iterations",
+    )
 
     args = parser.parse_args()
-
-
 
     save_dir = f"prado/results_{dt.datetime.now().strftime('%Y%m%d_%H%M%S')}"
     if not os.path.isdir(save_dir):
@@ -422,7 +458,13 @@ if __name__ == '__main__':
             "n_obs": 520,
             "size1": 15,
             "mu0": [0] * 5,
-            "sigma0": [0.08, 0.003, 0.01, 0.005, 0.007], # (crypto, bond, stock, forex, commodities)
+            "sigma0": [
+                0.08,
+                0.003,
+                0.01,
+                0.005,
+                0.007,
+            ],  # (crypto, bond, stock, forex, commodities)
             "sigma1F": 0.25,
             "sLength": 260,
             "process": "garch",
@@ -430,5 +472,11 @@ if __name__ == '__main__':
     else:
         raise NotImplementedError(args.dgp)
 
-    mc_hrp(METHODS_MAPPER, args.dgp,  dgp_params, n_jobs=args.n_jobs,
-           num_iters=args.num_iters, save_dir=save_dir)
+    mc_hrp(
+        METHODS_MAPPER,
+        args.dgp,
+        dgp_params,
+        n_jobs=args.n_jobs,
+        num_iters=args.num_iters,
+        save_dir=save_dir,
+    )
