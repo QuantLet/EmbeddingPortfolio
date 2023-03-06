0c87d00b097aa98a63f1beafc30cc964d7792a07
BrunoSpilak <bruno.spilak@gmail.com>
2023-03-06 10:41:18 +0100

    format

--- dl_portfolio/alm.py
+++ dl_portfolio/alm.py
@@ -17,18 +17,18 @@ from scipy.optimize import fmin_bfgs
 # some global constants
 _epsilon = np.sqrt(np.finfo(float).eps)  # the float number accuracy
 # standard status messages of optimizers
-_status_message = {'success': 'Optimization terminated successfully.',
-                   'maxfev': 'Maximum number of function evaluations has '
-                             'been exceeded.',
-                   'maxiter': 'Maximum number of iterations has been '
-                              'exceeded.',
-                   'pr_loss': 'Desired error not necessarily achieved due '
-                              'to precision loss.'}
+_status_message = {
+    "success": "Optimization terminated successfully.",
+    "maxfev": "Maximum number of function evaluations has " "been exceeded.",
+    "maxiter": "Maximum number of iterations has been " "exceeded.",
+    "pr_loss": "Desired error not necessarily achieved due "
+    "to precision loss.",
+}
 
 
 # miscellaneous functions
 class OptimizeResult(dict):
-    """ Represents the optimization result.
+    """Represents the optimization result.
 
     Attributes
     ----------
@@ -77,8 +77,9 @@ class OptimizeResult(dict):
     def __repr__(self):
         if self.keys():
             m = max(map(len, list(self.keys()))) + 1
-            return '\n'.join([k.rjust(m) + ': ' + repr(v)
-                              for k, v in sorted(self.items())])
+            return "\n".join(
+                [k.rjust(m) + ": " + repr(v) for k, v in sorted(self.items())]
+            )
         else:
             return self.__class__.__name__ + "()"
 
@@ -116,7 +117,8 @@ def wrap_function(function, args):
 
     def function_wrapper(*wrapper_args):
         ncalls[
-            0] += 1  # record the number of times that the function is called.
+            0
+        ] += 1  # record the number of times that the function is called.
         return function(*(wrapper_args + args))
 
     return ncalls, function_wrapper
@@ -148,25 +150,49 @@ def ALMfunc(x, objFun, conFuns, w, v, sigma):
     ineqConFuns, eqConFuns = conFuns(x)
     if (len(ineqConFuns) != len(w)) or (len(eqConFuns) != len(v)):
         print(
-            "The multiplier number is not equal to that of the corresponding constraints!")
+            "The multiplier number is not equal to that of the corresponding constraints!"
+        )
         exit(-1)
     sumValue1 = np.sum(
-        [(np.max([0, wi - sigma * ineqi])) ** 2 - wi ** 2 for wi, ineqi in
-         zip(w, ineqConFuns)])
-    sumValue2 = np.sum([0.5 * sigma * ((eqj) ** 2) - vj * eqj for vj, eqj in
-                        zip(v, eqConFuns)])
-    return objFun(x) + sumValue1 / (2. * sigma) + sumValue2
+        [
+            (np.max([0, wi - sigma * ineqi])) ** 2 - wi ** 2
+            for wi, ineqi in zip(w, ineqConFuns)
+        ]
+    )
+    sumValue2 = np.sum(
+        [
+            0.5 * sigma * ((eqj) ** 2) - vj * eqj
+            for vj, eqj in zip(v, eqConFuns)
+        ]
+    )
+    return objFun(x) + sumValue1 / (2.0 * sigma) + sumValue2
 
 
 class _UnconstrainedOptimizationError(RuntimeError):
     pass
 
 
-def fmin_ALM(objFun, x0, conFuns, eqConNum, ineqConNum, \
-             funArgs=(), conArgs=(), multiplierRange=(0.1, 1.0), \
-             gtol=1e-5, sigma=3.0, beta=0.9, alpha=1.3, \
-             norm=2, epsilon=_epsilon, maxiter=None, full_output=0, \
-             disp=1, retall=0, callback=None):
+def fmin_ALM(
+    objFun,
+    x0,
+    conFuns,
+    eqConNum,
+    ineqConNum,
+    funArgs=(),
+    conArgs=(),
+    multiplierRange=(0.1, 1.0),
+    gtol=1e-5,
+    sigma=3.0,
+    beta=0.9,
+    alpha=1.3,
+    norm=2,
+    epsilon=_epsilon,
+    maxiter=None,
+    full_output=0,
+    disp=1,
+    retall=0,
+    callback=None,
+):
     """
     The optimization algorithm application interface function.
 
@@ -192,33 +218,71 @@ def fmin_ALM(objFun, x0, conFuns, eqConNum, ineqConNum, \
     allvecs  :  list
         `OptimizeResult` at each iteration.  Only returned if retall is True.
     """
-    res = _fmin_ALM(objFun, x0, conFuns, eqConNum, ineqConNum, \
-                    funArgs, conArgs, multiplierRange, \
-                    gtol, sigma, beta, alpha, \
-                    norm, epsilon, maxiter, full_output, \
-                    disp, retall, callback)
+    res = _fmin_ALM(
+        objFun,
+        x0,
+        conFuns,
+        eqConNum,
+        ineqConNum,
+        funArgs,
+        conArgs,
+        multiplierRange,
+        gtol,
+        sigma,
+        beta,
+        alpha,
+        norm,
+        epsilon,
+        maxiter,
+        full_output,
+        disp,
+        retall,
+        callback,
+    )
     # fun=fval, nfev=func_calls[0], nconev=conFunc_calls[0],\
     # status=warnflag, success=(warnflag == 0), message=msg,\
     # x=xk, nit=k
     if full_output:
-        retlist = (res['x'], res['fun'], \
-                   res['nfev'], res['nconev'], res['status'], res['message'], \
-                   res['nit'])
+        retlist = (
+            res["x"],
+            res["fun"],
+            res["nfev"],
+            res["nconev"],
+            res["status"],
+            res["message"],
+            res["nit"],
+        )
         if retall:
-            retlist += (res['allvecs'],)
+            retlist += (res["allvecs"],)
         return retlist
     else:
         if retall:
-            return res['x'], res['allvecs']
+            return res["x"], res["allvecs"]
         else:
-            return res['x']
+            return res["x"]
 
 
-def _fmin_ALM(objFun, x0, conFuns, eqConNum, ineqConNum, \
-              funArgs=(), conArgs=(), multiplierRange=(0.1, 1.0), \
-              gtol=1e-5, sigma=3.0, beta=0.9, alpha=1.3, \
-              norm=2, epsilon=_epsilon, maxiter=None, full_output=0, \
-              disp=1, retall=0, callback=None):
+def _fmin_ALM(
+    objFun,
+    x0,
+    conFuns,
+    eqConNum,
+    ineqConNum,
+    funArgs=(),
+    conArgs=(),
+    multiplierRange=(0.1, 1.0),
+    gtol=1e-5,
+    sigma=3.0,
+    beta=0.9,
+    alpha=1.3,
+    norm=2,
+    epsilon=_epsilon,
+    maxiter=None,
+    full_output=0,
+    disp=1,
+    retall=0,
+    callback=None,
+):
     """
     The main entrance function of the optimization algorithm based on the
     augmented Lagrangian method.
@@ -280,18 +344,21 @@ def _fmin_ALM(objFun, x0, conFuns, eqConNum, ineqConNum, \
         x0.shape = (1,)
     if maxiter is None:
         maxiter = len(x0) * 200
-    opts = {'gtol': gtol,
-            'norm': norm,
-            'epsilon': epsilon,
-            'maxiter': maxiter,
-            'full_output': 1,
-            'disp': disp,
-            'retall': 1,
-            'callback': None}
+    opts = {
+        "gtol": gtol,
+        "norm": norm,
+        "epsilon": epsilon,
+        "maxiter": maxiter,
+        "full_output": 1,
+        "disp": disp,
+        "retall": 1,
+        "callback": None,
+    }
 
     func_calls, f = wrap_function(objFun, funArgs)  # f is callable, i.e., f(x)
-    conFunc_calls, conF = wrap_function(conFuns,
-                                        conArgs)  # conf in each list returned by conF(x) is callable
+    conFunc_calls, conF = wrap_function(
+        conFuns, conArgs
+    )  # conf in each list returned by conF(x) is callable
     allvecs = []
 
     gnorm = 1
@@ -305,15 +372,15 @@ def _fmin_ALM(objFun, x0, conFuns, eqConNum, ineqConNum, \
     if ineqConNum == 0:
         wk = np.array([])
     else:
-        wk = multiplierRange[0] + (multiplierRange[1] \
-                                   - multiplierRange[0]) * np.random.rand(
-            ineqConNum)
+        wk = multiplierRange[0] + (
+            multiplierRange[1] - multiplierRange[0]
+        ) * np.random.rand(ineqConNum)
     if eqConNum == 0:
         vk = np.array([])
     else:
-        vk = multiplierRange[0] + (multiplierRange[1] \
-                                   - multiplierRange[0]) * np.random.rand(
-            eqConNum)
+        vk = multiplierRange[0] + (
+            multiplierRange[1] - multiplierRange[0]
+        ) * np.random.rand(eqConNum)
 
     xk_1 = x0
     warnflag = 0
@@ -330,8 +397,9 @@ def _fmin_ALM(objFun, x0, conFuns, eqConNum, ineqConNum, \
             res = fmin_bfgs(ALfun, xk_1, fprime=None, args=(), **opts)
             if disp:
                 print(
-                    "The status of the %i-th unconstrained optimization: %s" % (
-                    k, res[6]))
+                    "The status of the %i-th unconstrained optimization: %s"
+                    % (k, res[6])
+                )
             if res[6] in [2, 3]:
                 print(f"Stop evaluation: warningflag is {res[6]}.")
                 break
@@ -349,12 +417,16 @@ def _fmin_ALM(objFun, x0, conFuns, eqConNum, ineqConNum, \
         # check whether the global tolerance is satisfied.
         if gnorm < gtol:
             print(
-                """The equality constraint norm evaluated at x[%i] is less than the global tolerance!""" % k)
+                """The equality constraint norm evaluated at x[%i] is less than the global tolerance!"""
+                % k
+            )
             break
         # check the objective value
         if not np.isfinite(fval):
             print(
-                """The function value evaluated at x[%i] is infinite! Something may go wrong!""" % k)
+                """The function value evaluated at x[%i] is infinite! Something may go wrong!"""
+                % k
+            )
             warnflag = 2
             break
         if retall:
@@ -377,7 +449,7 @@ def _fmin_ALM(objFun, x0, conFuns, eqConNum, ineqConNum, \
         warnflag = 2
 
     if warnflag == 2:
-        msg = _status_message['pr_loss']
+        msg = _status_message["pr_loss"]
         if disp:
             print("Warning: " + msg)
             print("         Current function value: %f" % fval)
@@ -386,7 +458,7 @@ def _fmin_ALM(objFun, x0, conFuns, eqConNum, ineqConNum, \
             print("         Constraint evaluations: %d" % conFunc_calls[0])
     elif k >= maxiter:
         warnflag = 1
-        msg = _status_message['maxiter']
+        msg = _status_message["maxiter"]
         if disp:
             print("Warning: " + msg)
             print("         Current function value: %f" % fval)
@@ -394,7 +466,7 @@ def _fmin_ALM(objFun, x0, conFuns, eqConNum, ineqConNum, \
             print("         Function evaluations: %d" % func_calls[0])
             print("         Constraint evaluations: %d" % conFunc_calls[0])
     else:
-        msg = _status_message['success']
+        msg = _status_message["success"]
         if disp:
             print("Warning: " + msg)
             print("         Current function value: %f" % fval)
@@ -402,13 +474,18 @@ def _fmin_ALM(objFun, x0, conFuns, eqConNum, ineqConNum, \
             print("         Function evaluations: %d" % func_calls[0])
             print("         Constraint evaluations: %d" % conFunc_calls[0])
 
-    result = OptimizeResult(fun=fval, nfev=func_calls[0],
-                            nconev=conFunc_calls[0], \
-                            status=warnflag, success=(warnflag == 0),
-                            message=msg, \
-                            x=xk, nit=k)
+    result = OptimizeResult(
+        fun=fval,
+        nfev=func_calls[0],
+        nconev=conFunc_calls[0],
+        status=warnflag,
+        success=(warnflag == 0),
+        message=msg,
+        x=xk,
+        nit=k,
+    )
     if retall:
-        result['allvecs'] = allvecs
+        result["allvecs"] = allvecs
     return result
 
 
@@ -484,20 +561,26 @@ def testConFun(x, parameters):
     ineqConsArray = []
     eqConsArray = []
     for keyStr, paraList in parameters.items():
-        if keyStr == 'ineq':
+        if keyStr == "ineq":
             for paraTuple in paraList:
                 ineqConsArray.append(
-                    paraTuple[0] * x[0] + paraTuple[1] * x[1] + paraTuple[2])
-        elif keyStr == 'eq':
+                    paraTuple[0] * x[0] + paraTuple[1] * x[1] + paraTuple[2]
+                )
+        elif keyStr == "eq":
             for paraTuple in paraList:
                 eqConsArray.append(
-                    paraTuple[0] * x[0] + paraTuple[1] * x[1] + paraTuple[2])
+                    paraTuple[0] * x[0] + paraTuple[1] * x[1] + paraTuple[2]
+                )
         else:
-            print("The parameter dictionary of the constraints does not contain \
-            the key '%s'!" % keyStr)
+            print(
+                "The parameter dictionary of the constraints does not contain \
+            the key '%s'!"
+                % keyStr
+            )
             exit(-1)
-    return np.asarray(ineqConsArray, dtype="float"), np.asarray(eqConsArray,
-                                                                dtype="float")
+    return np.asarray(ineqConsArray, dtype="float"), np.asarray(
+        eqConsArray, dtype="float"
+    )
 
 
 if __name__ == "__main__":
@@ -505,10 +588,15 @@ if __name__ == "__main__":
 
     x0 = np.array([2, 0])
     funParameters = np.array([1, 2.5])
-    ineqParameters = [(1, -2, 2), (-1, -2, 6), (-1, 2, 2), (1, 0, 0),
-                      (0, 1, 0)]
+    ineqParameters = [
+        (1, -2, 2),
+        (-1, -2, 6),
+        (-1, 2, 2),
+        (1, 0, 0),
+        (0, 1, 0),
+    ]
     eqParameters = [(1.7, 0.6, -3.4)]
-    conParaDict = {'ineq': ineqParameters, 'eq': eqParameters}
+    conParaDict = {"ineq": ineqParameters, "eq": eqParameters}
     # evaluate the function as well as the constraint functions at the initial point
     fval0 = testObjFun(x0, funParameters)
     ineqVals, eqVals = testConFun(x0, conParaDict)
@@ -521,12 +609,27 @@ if __name__ == "__main__":
     print("------Begin testing------")
     retall = 1
     bt = time.time()
-    res = fmin_ALM(testObjFun, x0, testConFun, eqConNum=1, ineqConNum=5, \
-                   funArgs=(funParameters,), conArgs=(conParaDict,),
-                   multiplierRange=(0.1, 1.0), \
-                   gtol=1e-5, sigma=3.0, beta=0.9, alpha=1.3, \
-                   norm=2, epsilon=_epsilon, maxiter=None, full_output=1, \
-                   disp=0, retall=retall, callback=None)
+    res = fmin_ALM(
+        testObjFun,
+        x0,
+        testConFun,
+        eqConNum=1,
+        ineqConNum=5,
+        funArgs=(funParameters,),
+        conArgs=(conParaDict,),
+        multiplierRange=(0.1, 1.0),
+        gtol=1e-5,
+        sigma=3.0,
+        beta=0.9,
+        alpha=1.3,
+        norm=2,
+        epsilon=_epsilon,
+        maxiter=None,
+        full_output=1,
+        disp=0,
+        retall=retall,
+        callback=None,
+    )
     print("------End testing------")
     print("Time cost: %s[s]" % (time.time() - bt))
     print("The optimal point is (%s, %s)" % (res[0][0], res[0][1]))
@@ -535,32 +638,44 @@ if __name__ == "__main__":
         import matplotlib.pyplot as plt
 
         plt.figure(0, figsize=(10, 8))
-        plt.plot([xf[1] for xf in res[-1]], linewidth=3.0, \
-                 color='red', marker='o', linestyle='-', \
-                 markersize=20, fillstyle='none', \
-                 label='convergence to (%.3f,%.3f)' % (res[0][0], res[0][1]))
+        plt.plot(
+            [xf[1] for xf in res[-1]],
+            linewidth=3.0,
+            color="red",
+            marker="o",
+            linestyle="-",
+            markersize=20,
+            fillstyle="none",
+            label="convergence to (%.3f,%.3f)" % (res[0][0], res[0][1]),
+        )
 
-        plt.title('Ideal optimal point: (1.4,1.7)', fontsize=28, \
-                  fontname='Times New Roman', color='m')
-        plt.xlabel('Iteration', fontsize=28, fontname='Times New Roman')
-        plt.ylabel('Objective function value', fontsize=28, \
-                   fontname='Times New Roman')
+        plt.title(
+            "Ideal optimal point: (1.4,1.7)",
+            fontsize=28,
+            fontname="Times New Roman",
+            color="m",
+        )
+        plt.xlabel("Iteration", fontsize=28, fontname="Times New Roman")
+        plt.ylabel(
+            "Objective function value", fontsize=28, fontname="Times New Roman"
+        )
 
-        legendfont = {"family": "Times New Roman", \
-                      "size": 23}
+        legendfont = {"family": "Times New Roman", "size": 23}
         plt.legend(prop=legendfont, bbox_to_anchor=(0.95, 0.28))
-        plt.xticks(fontsize=23, fontname='serif')
-        plt.yticks(fontsize=23, fontname='serif')
-        problemStr = ["$\min_{(x_1,x_2)}{f(x)=(x_1-1)^2+(x_2-2.5)^2}$", \
-                      " $x_1 - 2x_2 + 2 \geq0$", \
-                      " $-x_1 - 2x_2 + 6\geq0$", \
-                      " $-x_1 + 2x_2 + 2\geq0$", \
-                      " $1.7x_1 + 0.6x_2 - 3.4 = 0$", \
-                      " $x_1,x_2\geq0$"]
+        plt.xticks(fontsize=23, fontname="serif")
+        plt.yticks(fontsize=23, fontname="serif")
+        problemStr = [
+            "$\min_{(x_1,x_2)}{f(x)=(x_1-1)^2+(x_2-2.5)^2}$",
+            " $x_1 - 2x_2 + 2 \geq0$",
+            " $-x_1 - 2x_2 + 6\geq0$",
+            " $-x_1 + 2x_2 + 2\geq0$",
+            " $1.7x_1 + 0.6x_2 - 3.4 = 0$",
+            " $x_1,x_2\geq0$",
+        ]
         vdist = 0.8
         i = 0
         for strLatex in problemStr[:-1]:
-            plt.text(1, 6.6 - vdist * i, strLatex, fontsize=23, color='red')
+            plt.text(1, 6.6 - vdist * i, strLatex, fontsize=23, color="red")
             i += 1
         plt.grid(True)
         plt.show()
